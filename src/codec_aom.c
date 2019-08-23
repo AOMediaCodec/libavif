a78fa98a3ddbb45041e3f5eab3a2f15d8d374d75
Joe Drago <jdrago@netflix.com>
2019-08-23 16:56:00 -0700

    Fix warning

--- src/codec_aom.c
+++ src/codec_aom.c
@@ -158,7 +158,7 @@ static avifBool aomCodecGetNextImage(avifCodec * codec, avifImage * image)
             nclx.colourPrimaries = (uint16_t)codec->internal->image->cp;
             nclx.transferCharacteristics = (uint16_t)codec->internal->image->tc;
             nclx.matrixCoefficients = (uint16_t)codec->internal->image->mc;
-            nclx.fullRangeFlag = image->yuvRange;
+            nclx.fullRangeFlag = (uint8_t)image->yuvRange;
             avifImageSetProfileNCLX(image, &nclx);
         }
 
