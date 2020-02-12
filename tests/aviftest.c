c8ade7347f2f8d1641e2ee601ec4afc43b73b6dc
Joe Drago <jdrago@netflix.com>
2020-02-12 13:28:34 -0800

    Remove antiquated aviffuzz (as libavif uses oss-fuzz), stub out new aviftest and add to CI to test basic workflow

---
+++ tests/aviftest.c
@@ -0,0 +1,16 @@
+// Copyright 2020 Joe Drago. All rights reserved.
+// SPDX-License-Identifier: BSD-2-Clause
+
+#include "avif/avif.h"
+
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    printf("AVIF Test Suite: Running...\n");
+    printf("AVIF Test Suite: Complete.\n");
+    return 0;
+}
