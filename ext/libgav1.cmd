8d150b4981adf03a0ef6c7aef867f0245d99229b
wantehchang <wtc@google.com>
2020-02-21 14:08:38 -0800

    Add libgav1 support (decoder only). (#57)
    * Add libgav1 support (decoder only).
    
    Only the local, static library build (cmake -DAVIF_CODEC_LIBGAV1=ON
    -DAVIF_LOCAL_LIBGAV1=ON) has been tested. Since libgav1 is written in
    C++ internally, the executable programs need to be linked with the C++
    compiler.
    
    src/codec_libgav1.c is a port of src/codec_aom.c (the decoder portions)
    and src/codec_dav1d.c.
    
    Known issues:
    * cmake/Modules/Findlibgav1.cmake is not tested.
    * libgav1 does not support the "output all layers" option yet.

---
+++ ext/libgav1.cmd
@@ -0,0 +1,21 @@
+: # If you want to use a local build of libgav1, you must clone the libgav1 repo in this directory first, then enable CMake's AVIF_CODEC_LIBGAV1 and AVIF_LOCAL_LIBGAV1 options.
+: # The git SHA below is known to work, and will occasionally be updated. Feel free to use a more recent commit.
+
+: # The odd choice of comment style in this file is to try to share this script between *nix and win32.
+
+: # cmake and ninja must be in your PATH.
+
+: # If you're running this on Windows, be sure you've already run this (from your VC2017 install dir):
+: #     "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+
+git clone https://chromium.googlesource.com/codecs/libgav1
+
+cd libgav1
+git checkout 1fccd75
+git clone https://github.com/abseil/abseil-cpp.git third_party/abseil-cpp
+mkdir build
+cd build
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLIBGAV1_THREADPOOL_USE_STD_MUTEX=1 ..
+ninja
+cd ../..
