b8427508036d9255cdf8e89443a0d55b4945658b
Andreas Schneider <asn@cryptomilk.org>
2020-03-03 17:00:54 +0100

    cmake: Provide a pkgconfig file
    Fixes #80
    
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

---
+++ libavif.pc.cmake
@@ -0,0 +1,10 @@
+prefix=@CMAKE_INSTALL_FULL_PREFIX@
+exec_prefix=${prefix}/bin
+libdir=${prefix}/lib
+includedir=${prefix}/include
+
+Name: @PROJECT_NAME@
+Description: Library for encoding and decoding .avif files
+Version: @PROJECT_VERSION@
+Libs: -L${libdir} -lavif
+Cflags: -I{includedir}
