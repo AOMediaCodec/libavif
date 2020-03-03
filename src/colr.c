bfc5088851ef3b5ca5b73fd024846e5685c54860
psi <ryo.hirafuji@link-u.co.jp>
2020-03-03 19:03:23 +0900

    Support Chromaticity-derived non-constant luminance system (No. 12)

--- src/colr.c
+++ src/colr.c
@@ -423,6 +423,31 @@ static avifBool calcYUVInfoFromNCLX(avifNclxColorProfile * nclx, float coeffs[3]
         coeffs[2] = table->kb;
         coeffs[1] = 1.0f - coeffs[0] - coeffs[2];
         return AVIF_TRUE;
+      }else if(AVIF_NCLX_MATRIX_COEFFICIENTS_CHROMA_DERIVED_NCL == nclx->matrixCoefficients) {
+        float primaries[8];
+        avifNclxColourPrimariesGetValues(nclx->colourPrimaries, primaries);
+        float const rX = primaries[0];
+        float const rY = primaries[1];
+        float const gX = primaries[2];
+        float const gY = primaries[3];
+        float const bX = primaries[4];
+        float const bY = primaries[5];
+        float const wX = primaries[6];
+        float const wY = primaries[7];
+        float const rZ = 1.0f - (rX + rY); // (Eq. 34)
+        float const gZ = 1.0f - (gX + gY); // (Eq. 35)
+        float const bZ = 1.0f - (bX + bY); // (Eq. 36)
+        float const wZ = 1.0f - (wX + wY); // (Eq. 37)
+        float const kr = (rY*(wX*(gY*bZ - bY*gZ) + wY*(bX*gZ - gX*bZ) + wZ*(gX*bY - bX*gY)))
+                       / (wY*(rX*(gY*bZ - bY*gZ) + gX*(bY*rZ - rY*bZ) + bX*(rY*gZ - gY*rZ)));
+        // (Eq. 32)
+        float const kb = (bY*(wX*(rY*gZ - gY * rZ) + wY*(gX*rZ - rX*gZ) + wZ*(rX*gY - gX*rY)))
+                       / (wY*(rX*(gY*bZ - bY * gZ) + gX*(bY*rZ - rY*bZ) + bX*(rY*gZ - gY*rZ)));
+        // (Eq. 33)
+        coeffs[0] = kr;
+        coeffs[2] = kb;
+        coeffs[1] = 1.0f - coeffs[0] - coeffs[2];
+        return AVIF_TRUE;
       }
     }
     return AVIF_FALSE;
