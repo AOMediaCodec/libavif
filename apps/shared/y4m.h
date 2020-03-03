cc4d801d9a0ea90465507648d36953090740914f
wantehchang <wtc@google.com>
2020-03-03 10:55:48 -0800

    Miscellaneous header file cleanup. (#78)
    1. Add #ifndef header guard to apps/shared/avifutil.h and
    apps/shared/y4m.h.
    
    2. Do not install the internal header file include/avif/internal.h.
    It is only used by src/*.c. (It would be good to move it to the src/
    directory.)

--- apps/shared/y4m.h
+++ apps/shared/y4m.h
@@ -1,7 +1,12 @@
 // Copyright 2019 Joe Drago. All rights reserved.
 // SPDX-License-Identifier: BSD-2-Clause
 
+#ifndef LIBAVIF_APPS_SHARED_Y4M_H
+#define LIBAVIF_APPS_SHARED_Y4M_H
+
 #include "avif/avif.h"
 
 avifBool y4mRead(avifImage * avif, const char * inputFilename);
 avifBool y4mWrite(avifImage * avif, const char * outputFilename);
+
+#endif // ifndef LIBAVIF_APPS_SHARED_Y4M_H
