b84c94cc6fd70bfc7f482c0e9257826d3f49d43b
Jan Beich <jbeich@FreeBSD.org>
2020-03-04 21:02:36 +0000

    cmake: fix typos in pkg-config file

--- libavif.pc.cmake
+++ libavif.pc.cmake
@@ -1,4 +1,4 @@
-prefix=@CMAKE_INSTALL_FULL_PREFIX@
+prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}/bin
 libdir=${prefix}/lib
 includedir=${prefix}/include
@@ -7,4 +7,4 @@ Name: @PROJECT_NAME@
 Description: Library for encoding and decoding .avif files
 Version: @PROJECT_VERSION@
 Libs: -L${libdir} -lavif
-Cflags: -I{includedir}
+Cflags: -I${includedir}
