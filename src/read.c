9d36878159486fa8af150c89d3b7a7b3e4964722
Joe Drago <jdrago@netflix.com>
2020-03-04 17:53:17 -0800

    Forget pointers into codec image buffers in tiles when resetting the codec (benign)

--- src/read.c
+++ src/read.c
@@ -401,6 +401,9 @@ static void avifDataResetCodec(avifData * data)
 {
     for (unsigned int i = 0; i < data->tiles.count; ++i) {
         avifTile * tile = &data->tiles.tile[i];
+        if (tile->image) {
+            avifImageFreePlanes(tile->image, AVIF_PLANES_ALL); // forget any pointers into codec image buffers
+        }
         if (tile->codec) {
             avifCodecDestroy(tile->codec);
             tile->codec = NULL;
