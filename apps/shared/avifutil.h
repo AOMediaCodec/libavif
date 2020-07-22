edcb82b9a70e8c951449cceceec94757375bff5c
Vincent Torri <vincent.torri@gmail.com>
2020-07-22 06:32:49 +0200

    Add comment explaining the usage of AVIF_FMT_ZU

--- apps/shared/avifutil.h
+++ apps/shared/avifutil.h
@@ -6,6 +6,11 @@
 
 #include "avif/avif.h"
 
+/*
+ * With Visual Studio before 2013 and mingw-w64 before 2020 the 29th april,
+ * (and __USE_MINGW_ANSI_STDIO not set to 1), %z precision specifier is not
+ * supported. Hence %I must be used. %I is on the other hand always supported.
+ */
 #ifdef _WIN32
 # define AVIF_FMT_ZU "%Iu"
 #else
