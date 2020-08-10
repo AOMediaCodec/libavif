37dd1a6e6b68706b3e8726c8f2090bd8dbade69e
Wan-Teh Chang <wtc@google.com>
2020-08-10 13:29:58 -0700

    Update codec_dav1d.c

--- src/codec_dav1d.c
+++ src/codec_dav1d.c
@@ -207,6 +207,6 @@ avifCodec * avifCodecCreateDav1d(void)
     memset(codec->internal, 0, sizeof(struct avifCodecInternal));
     dav1d_default_settings(&codec->internal->dav1dSettings);
     // Set a maximum frame size limit to avoid OOM'ing fuzzers.
-    codec->internal->dav1dSettings.frame_size_limit = 16384 * 16384;
+    codec->internal->dav1dSettings.frame_size_limit = AVIF_MAX_IMAGE_SIZE;
     return codec;
 }
