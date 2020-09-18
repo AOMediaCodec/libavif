762b59f80b39a416e79fe4effedccefebef9d590
Wan-Teh Chang <wtc@google.com>
2020-09-18 12:48:05 -0700

    Add four more aom-specific options
    Add the aom-specific options aq-mode, cq-level, end-usage, and
    sharpness.
    
    Adapt some of the command-line argument processing code in libaom for
    use in src/codec_aom.c. Git history shows that the libaom code that I
    adapted was all written by Google employees, originally by John Koleszar
    in 2012. Here is a table of the libaom code that I adapted:
        libaom                           src/codec_aom.c
        =========================================================
        The "arg" prefix                 The "aomOption" prefix
        struct arg_enum_list             struct aomOptionEnumList
        end_usage_enum                   endUsageEnum
        tuning_enum                      tuningEnum
        struct arg_def                   struct aomOptionDef
        av1_args and av1_arg_ctrl_map    aomOptionDefs
        arg_parse_int()                  aomOptionParseInt()
        arg_parse_enum()                 aomOptionParseEnum()

--- apps/avifenc.c
+++ apps/avifenc.c
@@ -94,6 +94,10 @@ static void syntax(void)
     printf("\n");
     if (avifCodecName(AVIF_CODEC_CHOICE_AOM, 0)) {
         printf("aom-specific advanced options:\n");
+        printf("    aq-mode=M                         : Adaptive quantization mode (0: off (default), 1: variance, 2: complexity, 3: cyclic refresh)\n");
+        printf("    cq-level=Q                        : Constant/Constrained Quality level (0-63, end-usage must be set to cq or q)\n");
+        printf("    end-usage=MODE                    : Rate control mode (vbr, cbr, cq, or q)\n");
+        printf("    sharpness=S                       : Loop filter sharpness (0-7, default: 0)\n");
         printf("    tune=METRIC                       : Tune the encoder for distortion metric (psnr or ssim, default: psnr)\n");
         printf("\n");
     }
