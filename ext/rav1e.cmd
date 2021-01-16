9860c73c0c7d9e880758d7342da0a92ba4a5ba66
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2021-01-16 14:53:10 +0100

    rav1e.cmd: Use new install method for C-API
    rav1e switched from cbindgen to cargo-c some time ago. With 0.4 the old method broke, so this commit replaces it with the new one.
    
    See: https://github.com/xiph/rav1e#building-the-c-api

--- ext/rav1e.cmd
+++ ext/rav1e.cmd
@@ -14,8 +14,6 @@
 git clone -b 0.4 --depth 1 https://github.com/xiph/rav1e.git
 
 cd rav1e
-cargo install cbindgen
-cbindgen -c cbindgen.toml -l C -o target/release/include/rav1e/rav1e.h --crate rav1e .
-
-cargo build --lib --release --features capi
+cargo install cargo-c
+cargo cinstall --release
 cd ..
