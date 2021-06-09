f7f96517b809a55ddb12039c3255629c16f83f14
Wan-Teh Chang <wtc@google.com>
2021-06-09 07:04:21 -0700

    ext: Checkout libaom v3.1.1
    Updates the libaom version checked out in the ext and docker scripts
    from v3.1.0 to v3.1.1.

--- tests/docker/build.sh
+++ tests/docker/build.sh
@@ -34,7 +34,7 @@ nasm --version
 
 # aom
 cd
-git clone -b v3.1.0 --depth 1 https://aomedia.googlesource.com/aom
+git clone -b v3.1.1 --depth 1 https://aomedia.googlesource.com/aom
 cd aom
 mkdir build.avif
 cd build.avif
