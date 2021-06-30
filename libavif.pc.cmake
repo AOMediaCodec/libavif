c93a6a46c15ae4cddd9e542de92d7aa3f4ba8e61
Miloš Komarčević <4973094+kmilos@users.noreply.github.com>
2021-06-30 23:13:19 +0200

    Update libavif.pc.cmake

--- libavif.pc.cmake
+++ libavif.pc.cmake
@@ -1,10 +1,10 @@
 prefix=@CMAKE_INSTALL_PREFIX@
-exec_prefix=${prefix}/bin
-libdir=${prefix}/@CMAKE_INSTALL_LIBDIR@
-includedir=${prefix}/include
+exec_prefix=${prefix}
+libdir=${exec_prefix}/@CMAKE_INSTALL_LIBDIR@
+includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
 
 Name: @PROJECT_NAME@
 Description: Library for encoding and decoding .avif files
 Version: @PROJECT_VERSION@
 Libs: -L${libdir} -lavif
-Cflags: -I${includedir}@AVIF_PKG_CONFIG_EXTRA_CFLAGS@
+Cflags: -I${includedir}/avif@AVIF_PKG_CONFIG_EXTRA_CFLAGS@
