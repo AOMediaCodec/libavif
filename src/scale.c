0e3ffa094015d924214b6617ccb5b7c5c4882cd4
Frank Galligan <frankgalligan@gmail.com>
2021-08-11 11:08:16 -0700

    Match style of libavif

--- src/scale.c
+++ src/scale.c
@@ -64,9 +64,9 @@ avifBool avifImageScale(avifImage * image, uint32_t dstWidth, uint32_t dstHeight
     image->height = dstHeight;
 
     if (srcYUVPlanes[0]) {
-        const int fixed_point_div = (int)(((int64_t)(srcWidth) << 16) / dstWidth);
-        const int64_t max_fixed_value = fixed_point_div * (dstWidth - 1);
-        if (max_fixed_value > 0x7fffffff) {
+        const int fixedPointDiv = (int)(((int64_t)(srcWidth) << 16) / dstWidth);
+        const int64_t maxFixedValue = fixedPointDiv * (dstWidth - 1);
+        if (maxFixedValue > 0x7fffffff) {
             avifDiagnosticsPrintf(diag, "avifImageScale requested invalid width scale for libyuv [%u -> %u]", srcWidth, dstWidth);
             return AVIF_FALSE;
         }
@@ -109,9 +109,9 @@ avifBool avifImageScale(avifImage * image, uint32_t dstWidth, uint32_t dstHeight
     }
 
     if (srcAlphaPlane) {
-        const int fixed_point_div = (int)(((int64_t)(srcWidth) << 16) / dstWidth);
-        const int64_t max_fixed_value = fixed_point_div * (dstWidth - 1);
-        if (max_fixed_value > 0x7fffffff) {
+        const int fixedPointDiv = (int)(((int64_t)(srcWidth) << 16) / dstWidth);
+        const int64_t maxFixedValue = fixedPointDiv * (dstWidth - 1);
+        if (maxFixedValue > 0x7fffffff) {
             avifDiagnosticsPrintf(diag, "avifImageScale requested invalid width scale for libyuv [%u -> %u]", srcWidth, dstWidth);
             return AVIF_FALSE;
         }
