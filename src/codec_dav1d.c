db28773efda82ac61df0ec18c71f6287ed5cddde
Michael Bradshaw <mjbshaw@google.com>
2021-09-04 10:07:01 -0700

    Update codec_dav1d.c for the new threading model
    See https://code.videolan.org/videolan/dav1d/-/commit/753eef833b:
    Merge the 3 threading models into a single one
    by Victorien Le Couviour--Tuffet <victorien@videolan.org>

--- src/codec_dav1d.c
+++ src/codec_dav1d.c
@@ -57,8 +57,13 @@ static avifBool dav1dCodecGetNextImage(struct avifCodec * codec,
 {
     if (codec->internal->dav1dContext == NULL) {
         // Give all available threads to decode a single frame as fast as possible
+#if DAV1D_API_VERSION_MAJOR >= 6
+        codec->internal->dav1dSettings.max_frame_delay = 1;
+        codec->internal->dav1dSettings.n_threads = AVIF_CLAMP(decoder->maxThreads, 1, DAV1D_MAX_THREADS);
+#else
         codec->internal->dav1dSettings.n_frame_threads = 1;
         codec->internal->dav1dSettings.n_tile_threads = AVIF_CLAMP(decoder->maxThreads, 1, DAV1D_MAX_TILE_THREADS);
+#endif  // DAV1D_API_VERSION_MAJOR >= 6
         // Set a maximum frame size limit to avoid OOM'ing fuzzers. In 32-bit builds, if
         // frame_size_limit > 8192 * 8192, dav1d reduces frame_size_limit to 8192 * 8192 and logs
         // a message, so we set frame_size_limit to at most 8192 * 8192 to avoid the dav1d_log
