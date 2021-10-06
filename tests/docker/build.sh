5799d4fe6f2c525a1711c3e5782d6ffdc4497de3
Wan-Teh Chang <wtc@google.com>
2021-10-05 17:34:18 -0700

    ext: Checkout libaom v3.1.3
    Updates the libaom version checked out in the ext and docker scripts
    from v3.1.2 to v3.1.3.
    
    This fixes the build failure on macOS with the Apple M1 chip. See
    https://github.com/AOMediaCodec/libavif/pull/759.

--- tests/docker/build.sh
+++ tests/docker/build.sh
@@ -34,7 +34,7 @@ nasm --version
 
 # aom
 cd
-git clone -b v3.1.2 --depth 1 https://aomedia.googlesource.com/aom
+git clone -b v3.1.3 --depth 1 https://aomedia.googlesource.com/aom
 cd aom
 mkdir build.avif
 cd build.avif
