cfd9b6301009b1fb46d185fc0e92f26d068746f1
Wan-Teh Chang <wtc@google.com>
2022-02-10 14:18:09 -0800

    avifArrayPop() should zero the popped element
    avifArrayPop() should zero the memor of the popped element. If we ever
    push an element to the array, the memory of the new element will be all
    zeros.
    
    Also make sure we cast arr->elementSize to size_t when we multiply it by
    another uint32_t variable.

--- src/utils.c
+++ src/utils.c
@@ -115,7 +115,7 @@ void * avifArrayPushPtr(void * arrayStruct)
 {
     uint32_t index = avifArrayPushIndex(arrayStruct);
     avifArrayInternal * arr = (avifArrayInternal *)arrayStruct;
-    return &arr->ptr[index * arr->elementSize];
+    return &arr->ptr[index * (size_t)arr->elementSize];
 }
 
 void avifArrayPush(void * arrayStruct, void * element)
@@ -129,6 +129,7 @@ void avifArrayPop(void * arrayStruct)
 {
     avifArrayInternal * arr = (avifArrayInternal *)arrayStruct;
     --arr->count;
+    memset(&arr->ptr[arr->count * (size_t)arr->elementSize], 0, arr->elementSize);
 }
 
 void avifArrayDestroy(void * arrayStruct)
