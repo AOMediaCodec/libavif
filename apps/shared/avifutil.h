ac769a1288d0086ece4631f81c3063c730f669b0
Yuan Tong <tongyuan200097@gmail.com>
2022-03-16 02:47:46 +0800

    Fix -Wformat / -Wformat-non-iso on MinGW UCRT (#874)

--- apps/shared/avifutil.h
+++ apps/shared/avifutil.h
@@ -6,14 +6,19 @@
 
 #include "avif/avif.h"
 
-// The %z format specifier is not available with Visual Studios before 2013 and mingw-w64 toolchains
-// with `__USE_MINGW_ANSI_STDIO` not set to 1. Hence the %I format specifier must be used instead
-// to print out `size_t`. Newer Visual Studios and mingw-w64 toolchains built with the commit
-// mentioned with c99 set as the standard supports the %z specifier properly.
+// The %z format specifier is not available in the old Windows CRT msvcrt,
+// hence the %I format specifier must be used instead to print out `size_t`.
+// The new Windows CRT UCRT, which is used by Visual Studio 2015 or later,
+// supports the %z specifier properly.
+//
+// Additionally, with c99 set as the standard mingw-w64 toolchains built with
+// the commit mentioned can patch format functions to support the %z specifier,
+// even if it's using the old msvcrt, and this can be detected by
+// the `__USE_MINGW_ANSI_STDIO` macro.
 //
 // Related mingw-w64 commit: bfd33f6c0ec5e652cc9911857dd1492ece8d8383
 
-#if (defined(_MSVC) && _MSVC < 1800) || (defined(__USE_MINGW_ANSI_STDIO) && __USE_MINGW_ANSI_STDIO == 0)
+#if !defined(_UCRT) && (defined(__USE_MINGW_ANSI_STDIO) && __USE_MINGW_ANSI_STDIO == 0)
 #define AVIF_FMT_ZU "%Iu"
 #else
 #define AVIF_FMT_ZU "%zu"
