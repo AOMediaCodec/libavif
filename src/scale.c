258974e8fd3010dd343807b92d423183f7a8a1ae
Wan-Teh Chang <wtc@google.com>
2022-04-01 12:49:47 -0700

    Run clang-format

--- src/scale.c
+++ src/scale.c
@@ -26,7 +26,7 @@ avifBool avifImageScale(avifImage * image, uint32_t dstWidth, uint32_t dstHeight
 // in version 1813:
 // https://chromium-review.googlesource.com/c/libyuv/libyuv/+/3183182
 // https://chromium-review.googlesource.com/c/libyuv/libyuv/+/3527834
-#pragma clang diagnostic ignored "-Wnewline-eof" // "no newline at end of file"
+#pragma clang diagnostic ignored "-Wnewline-eof"       // "no newline at end of file"
 #endif
 #include <libyuv.h>
 #if defined(__clang__)
