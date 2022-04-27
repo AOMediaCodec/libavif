f5b8da59fc8b02d6648afa779544d8ed86c970f6
Wan-Teh Chang <wtc@google.com>
2022-04-27 14:30:12 -0700

    Use the comment that Yannis suggested

--- include/avif/avif.h
+++ include/avif/avif.h
@@ -178,7 +178,7 @@ typedef struct avifRWData
 #define AVIF_DATA_EMPTY { NULL, 0 }
 // clang-format on
 
-// These functions assume the avifRWData input is initialized.
+// The avifRWData input must be zero-initialized before being manipulated with these functions.
 AVIF_API void avifRWDataRealloc(avifRWData * raw, size_t newSize);
 AVIF_API void avifRWDataSet(avifRWData * raw, const uint8_t * data, size_t len);
 AVIF_API void avifRWDataFree(avifRWData * raw);
