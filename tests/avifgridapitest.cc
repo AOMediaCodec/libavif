9b431859f897843d83b6b670516c90ff432e4046
Yannis Guyon <yguyon@google.com>
2022-06-15 15:37:38 +0200

    Shorten avifgridapitest and avify4mtest
    Move code to aviftest_helpers for clarity and to be reused by other tests.

--- tests/avifgridapitest.cc
+++ tests/avifgridapitest.cc
@@ -3,168 +3,20 @@
 
 #include "avif/avif.h"
 
-#include <cassert>
-#include <cinttypes>
-#include <cstdio>
-#include <cstring>
 #include <tuple>
+#include <vector>
 
+#include "aviftest_helpers.h"
 #include "gtest/gtest.h"
 
 using testing::Combine;
 using testing::Values;
 using testing::ValuesIn;
 
+namespace libavif
+{
 namespace
 {
-
-//------------------------------------------------------------------------------
-
-// Fills a plane with a repeating gradient.
-void fillPlane(int width, int height, int depth, uint8_t * row, uint32_t rowBytes)
-{
-    assert(depth == 8 || depth == 10 || depth == 12); // Values allowed by AV1.
-    const int maxValuePlusOne = 1 << depth;
-    for (int y = 0; y < height; ++y) {
-        if (depth == 8) {
-            memset(row, y % maxValuePlusOne, width);
-        } else {
-            for (int x = 0; x < width; ++x) {
-                ((uint16_t *)row)[x] = (uint16_t)(y % maxValuePlusOne);
-            }
-        }
-        row += rowBytes;
-    }
-}
-
-// Creates an image where the pixel values are defined but do not matter.
-// Returns false in case of memory failure.
-bool createImage(int width, int height, int depth, avifPixelFormat yuvFormat, bool createAlpha, avifImage ** image)
-{
-    *image = avifImageCreate(width, height, depth, yuvFormat);
-    if (!*image) {
-        printf("ERROR: avifImageCreate() failed\n");
-        return false;
-    }
-    avifImageAllocatePlanes(*image, createAlpha ? AVIF_PLANES_ALL : AVIF_PLANES_YUV);
-    if (width * height == 0) {
-        return true;
-    }
-
-    avifPixelFormatInfo formatInfo;
-    avifGetPixelFormatInfo((*image)->yuvFormat, &formatInfo);
-    uint32_t uvWidth = ((*image)->width + formatInfo.chromaShiftX) >> formatInfo.chromaShiftX;
-    uint32_t uvHeight = ((*image)->height + formatInfo.chromaShiftY) >> formatInfo.chromaShiftY;
-
-    const int planeCount = formatInfo.monochrome ? 1 : AVIF_PLANE_COUNT_YUV;
-    for (int plane = 0; plane < planeCount; ++plane) {
-        fillPlane((plane == AVIF_CHAN_Y) ? (*image)->width : uvWidth,
-                  (plane == AVIF_CHAN_Y) ? (*image)->height : uvHeight,
-                  (*image)->depth,
-                  (*image)->yuvPlanes[plane],
-                  (*image)->yuvRowBytes[plane]);
-    }
-
-    if (createAlpha) {
-        fillPlane((*image)->width, (*image)->height, (*image)->depth, (*image)->alphaPlane, (*image)->alphaRowBytes);
-    }
-    return true;
-}
-
-// Generates then encodes a grid image. Returns false in case of failure.
-bool encodeGrid(int columns, int rows, int cellWidth, int cellHeight, int depth, avifPixelFormat yuvFormat, bool createAlpha, avifRWData * output)
-{
-    bool success = false;
-    avifEncoder * encoder = NULL;
-    avifImage ** cellImages = (avifImage **)avifAlloc(sizeof(avifImage *) * columns * rows);
-    memset(cellImages, 0, sizeof(avifImage *) * columns * rows);
-    for (int iCell = 0; iCell < columns * rows; ++iCell) {
-        if (!createImage(cellWidth, cellHeight, depth, yuvFormat, createAlpha, &cellImages[iCell])) {
-            goto cleanup;
-        }
-    }
-
-    encoder = avifEncoderCreate();
-    if (!encoder) {
-        printf("ERROR: avifEncoderCreate() failed\n");
-        goto cleanup;
-    }
-    encoder->speed = AVIF_SPEED_FASTEST;
-    if (avifEncoderAddImageGrid(encoder, columns, rows, (const avifImage * const *)cellImages, AVIF_ADD_IMAGE_FLAG_SINGLE) !=
-        AVIF_RESULT_OK) {
-        printf("ERROR: avifEncoderAddImageGrid() failed\n");
-        goto cleanup;
-    }
-    if (avifEncoderFinish(encoder, output) != AVIF_RESULT_OK) {
-        printf("ERROR: avifEncoderFinish() failed\n");
-        goto cleanup;
-    }
-
-    success = true;
-cleanup:
-    if (encoder) {
-        avifEncoderDestroy(encoder);
-    }
-    if (cellImages) {
-        for (int i = 0; i < columns * rows; ++i) {
-            if (cellImages[i]) {
-                avifImageDestroy(cellImages[i]);
-            }
-        }
-        avifFree(cellImages);
-    }
-    return success;
-}
-
-//------------------------------------------------------------------------------
-
-// Decodes the data. Returns false in case of failure.
-bool decode(const avifRWData * encodedAvif)
-{
-    bool success = false;
-    avifImage * const image = avifImageCreateEmpty();
-    avifDecoder * const decoder = avifDecoderCreate();
-    if (!image || !decoder) {
-        printf("ERROR: memory allocation failed\n");
-        goto cleanup;
-    }
-    if (avifDecoderReadMemory(decoder, image, encodedAvif->data, encodedAvif->size) != AVIF_RESULT_OK) {
-        printf("ERROR: avifDecoderReadMemory() failed\n");
-        goto cleanup;
-    }
-    success = true;
-cleanup:
-    if (image) {
-        avifImageDestroy(image);
-    }
-    if (decoder) {
-        avifDecoderDestroy(decoder);
-    }
-    return success;
-}
-
-//------------------------------------------------------------------------------
-
-// Generates, encodes then decodes a grid image.
-bool encodeDecode(int columns, int rows, int cellWidth, int cellHeight, int depth, avifPixelFormat yuvFormat, bool createAlpha, bool expectedSuccess)
-{
-    bool success = false;
-    avifRWData encodedAvif = { nullptr, 0 };
-    if (encodeGrid(columns, rows, cellWidth, cellHeight, depth, yuvFormat, createAlpha, &encodedAvif) != expectedSuccess) {
-        goto cleanup;
-    }
-    // Only decode if the encoding was expected to succeed.
-    // Any successful encoding shall result in a valid decoding.
-    if (expectedSuccess && !decode(&encodedAvif)) {
-        goto cleanup;
-    }
-    success = true;
-cleanup:
-    avifRWDataFree(&encodedAvif);
-    return success;
-}
-
-//------------------------------------------------------------------------------
 
 // Pair of cell count and cell size for a single dimension.
 struct Cell
@@ -186,14 +38,41 @@ TEST_P(GridApiTest, EncodeDecode)
     const bool createAlpha = std::get<4>(GetParam());
     const bool expectedSuccess = std::get<5>(GetParam());
 
-    EXPECT_TRUE(encodeDecode(/*columns=*/horizontal.count,
-                             /*rows=*/vertical.count,
-                             /*cellWidth=*/horizontal.size,
-                             /*cellHeight=*/vertical.size,
-                             bitDepth,
-                             yuvFormat,
-                             createAlpha,
-                             expectedSuccess));
+    // Construct a grid.
+    std::vector<testutil::avifImagePtr> cellImages;
+    cellImages.reserve(horizontal.count * vertical.count);
+    for (int i = 0; i < horizontal.count * vertical.count; ++i) {
+        cellImages.emplace_back(
+            testutil::createImage(horizontal.size, vertical.size, bitDepth, yuvFormat, createAlpha ? AVIF_PLANES_ALL : AVIF_PLANES_YUV));
+        ASSERT_NE(cellImages.back(), nullptr);
+        testutil::fillImageGradient(cellImages.back().get());
+    }
+
+    // Encode the grid image.
+    testutil::avifEncoderPtr encoder(avifEncoderCreate(), avifEncoderDestroy);
+    ASSERT_NE(encoder, nullptr);
+    encoder->speed = AVIF_SPEED_FASTEST;
+    std::vector<avifImage *> cellImagePtrs(cellImages.size()); // Just here to match libavif API.
+    for (size_t i = 0; i < cellImages.size(); ++i) {
+        cellImagePtrs[i] = cellImages[i].get();
+    }
+    const avifResult result =
+        avifEncoderAddImageGrid(encoder.get(), horizontal.count, vertical.count, cellImagePtrs.data(), AVIF_ADD_IMAGE_FLAG_SINGLE);
+
+    if (expectedSuccess) {
+        ASSERT_EQ(result, AVIF_RESULT_OK);
+        testutil::avifRWDataCleaner encodedAvif;
+        ASSERT_EQ(avifEncoderFinish(encoder.get(), &encodedAvif), AVIF_RESULT_OK);
+
+        // Decode the grid image.
+        testutil::avifImagePtr image(avifImageCreateEmpty(), avifImageDestroy);
+        ASSERT_NE(image, nullptr);
+        testutil::avifDecoderPtr decoder(avifDecoderCreate(), avifDecoderDestroy);
+        ASSERT_NE(decoder, nullptr);
+        ASSERT_EQ(avifDecoderReadMemory(decoder.get(), image.get(), encodedAvif.data, encodedAvif.size), AVIF_RESULT_OK);
+    } else {
+        ASSERT_TRUE(result == AVIF_RESULT_INVALID_IMAGE_GRID || result == AVIF_RESULT_NO_CONTENT);
+    }
 }
 
 // A cell cannot be smaller than 64px in any dimension if there are several cells.
@@ -275,3 +154,4 @@ INSTANTIATE_TEST_SUITE_P(InvalidOddDimensions,
                                  /*expectedSuccess=*/Values(false)));
 
 } // namespace
+} // namespace libavif
--- tests/avify4mtest.cc
+++ tests/avify4mtest.cc
@@ -5,166 +5,19 @@
 
 #include "y4m.h"
 
-#include <cassert>
-#include <cinttypes>
-#include <cstdio>
-#include <cstring>
 #include <sstream>
 #include <tuple>
 
+#include "aviftest_helpers.h"
 #include "gtest/gtest.h"
 
 using testing::Combine;
 using testing::Values;
 
+namespace libavif
+{
 namespace
 {
-
-//------------------------------------------------------------------------------
-
-// Returns true if image1 and image2 are identical.
-bool compareYUVA(const avifImage * image1, const avifImage * image2)
-{
-    if (image1->width != image2->width || image1->height != image2->height || image1->depth != image2->depth ||
-        image1->yuvFormat != image2->yuvFormat || image1->yuvRange != image2->yuvRange) {
-        printf("ERROR: input mismatch\n");
-        return false;
-    }
-    assert(image1->width * image1->height > 0);
-
-    avifPixelFormatInfo formatInfo;
-    avifGetPixelFormatInfo(image1->yuvFormat, &formatInfo);
-    const uint32_t uvWidth = (image1->width + formatInfo.chromaShiftX) >> formatInfo.chromaShiftX;
-    const uint32_t uvHeight = (image1->height + formatInfo.chromaShiftY) >> formatInfo.chromaShiftY;
-
-    const int planeCount = formatInfo.monochrome ? 1 : AVIF_PLANE_COUNT_YUV;
-    for (int plane = 0; plane < planeCount; ++plane) {
-        const uint32_t widthByteCount =
-            ((plane == AVIF_CHAN_Y) ? image1->width : uvWidth) * ((image1->depth > 8) ? sizeof(uint16_t) : sizeof(uint8_t));
-        const uint32_t height = (plane == AVIF_CHAN_Y) ? image1->height : uvHeight;
-        const uint8_t * row1 = image1->yuvPlanes[plane];
-        const uint8_t * row2 = image2->yuvPlanes[plane];
-        for (uint32_t y = 0; y < height; ++y) {
-            if (memcmp(row1, row2, widthByteCount) != 0) {
-                printf("ERROR: different px at row %" PRIu32 ", channel %" PRIu32 "\n", y, plane);
-                return false;
-            }
-            row1 += image1->yuvRowBytes[plane];
-            row2 += image2->yuvRowBytes[plane];
-        }
-    }
-
-    if (image1->alphaPlane || image2->alphaPlane) {
-        if (!image1->alphaPlane || !image2->alphaPlane || image1->alphaPremultiplied != image2->alphaPremultiplied) {
-            printf("ERROR: input mismatch\n");
-            return false;
-        }
-        const uint32_t widthByteCount = image1->width * ((image1->depth > 8) ? sizeof(uint16_t) : sizeof(uint8_t));
-        const uint8_t * row1 = image1->alphaPlane;
-        const uint8_t * row2 = image2->alphaPlane;
-        for (uint32_t y = 0; y < image1->height; ++y) {
-            if (memcmp(row1, row2, widthByteCount) != 0) {
-                printf("ERROR: different px at row %" PRIu32 ", alpha\n", y);
-                return false;
-            }
-            row1 += image1->alphaRowBytes;
-            row2 += image2->alphaRowBytes;
-        }
-    }
-    return true;
-}
-
-//------------------------------------------------------------------------------
-
-// Fills each plane of the image with the maximum allowed value.
-void fillPlanes(avifImage * image)
-{
-    const uint16_t yuvValue = (image->yuvRange == AVIF_RANGE_LIMITED) ? (235 << (image->depth - 8)) : ((1 << image->depth) - 1);
-    avifPixelFormatInfo formatInfo;
-    avifGetPixelFormatInfo(image->yuvFormat, &formatInfo);
-    const int planeCount = formatInfo.monochrome ? 1 : AVIF_PLANE_COUNT_YUV;
-    for (int plane = 0; plane < planeCount; ++plane) {
-        if (image->yuvPlanes[plane]) {
-            const uint32_t planeWidth =
-                (plane == AVIF_CHAN_Y) ? image->width : ((image->width + formatInfo.chromaShiftX) >> formatInfo.chromaShiftX);
-            const uint32_t planeHeight =
-                (plane == AVIF_CHAN_Y) ? image->height : ((image->height + formatInfo.chromaShiftY) >> formatInfo.chromaShiftY);
-            for (uint32_t y = 0; y < planeHeight; ++y) {
-                uint8_t * const row = image->yuvPlanes[plane] + y * image->yuvRowBytes[plane];
-                if (image->depth == 8) {
-                    memset(row, yuvValue, planeWidth);
-                } else {
-                    for (uint32_t x = 0; x < planeWidth; ++x) {
-                        ((uint16_t *)row)[x] = yuvValue;
-                    }
-                }
-            }
-        }
-    }
-    if (image->alphaPlane) {
-        const uint16_t alphaValue = (1 << image->depth) - 1;
-        for (uint32_t y = 0; y < image->height; ++y) {
-            uint8_t * const row = image->alphaPlane + y * image->alphaRowBytes;
-            if (image->depth == 8) {
-                memset(row, alphaValue, image->width);
-            } else {
-                for (uint32_t x = 0; x < image->width; ++x) {
-                    ((uint16_t *)row)[x] = alphaValue;
-                }
-            }
-        }
-    }
-}
-
-// Creates an image and encodes then decodes it as a y4m file.
-bool encodeDecodeY4m(uint32_t width,
-                     uint32_t height,
-                     uint32_t depth,
-                     avifPixelFormat yuvFormat,
-                     avifRange yuvRange,
-                     bool createAlpha,
-                     const char filePath[])
-{
-    bool success = false;
-    avifImage * image = avifImageCreateEmpty();
-    avifImage * decoded = avifImageCreateEmpty();
-    if (!image || !decoded) {
-        printf("ERROR: avifImageCreate() failed\n");
-        goto cleanup;
-    }
-    image->width = width;
-    image->height = height;
-    image->depth = depth;
-    image->yuvFormat = yuvFormat;
-    image->yuvRange = yuvRange;
-    avifImageAllocatePlanes(image, createAlpha ? AVIF_PLANES_ALL : AVIF_PLANES_YUV);
-    fillPlanes(image);
-
-    if (!y4mWrite(filePath, image)) {
-        printf("ERROR: y4mWrite() failed\n");
-        goto cleanup;
-    }
-    if (!y4mRead(filePath, decoded, /*sourceTiming=*/NULL, /*iter=*/NULL)) {
-        printf("ERROR: y4mRead() failed\n");
-        goto cleanup;
-    }
-
-    if (!compareYUVA(image, decoded)) {
-        goto cleanup;
-    }
-
-    success = true;
-cleanup:
-    if (image) {
-        avifImageDestroy(image);
-    }
-    if (decoded) {
-        avifImageDestroy(decoded);
-    }
-    return success;
-}
-
-//------------------------------------------------------------------------------
 
 class Y4mTest
     : public testing::TestWithParam<std::tuple</*width=*/int, /*height=*/int, /*bitDepth=*/int, /*yuvFormat=*/avifPixelFormat, /*yuvRange=*/avifRange, /*createAlpha=*/bool>>
@@ -182,7 +35,22 @@ TEST_P(Y4mTest, EncodeDecode)
     std::ostringstream filePath;
     filePath << testing::TempDir() << "avify4mtest_" << width << "_" << height << "_" << bitDepth << "_" << yuvFormat << "_"
              << yuvRange << "_" << createAlpha;
-    EXPECT_TRUE(encodeDecodeY4m(width, height, bitDepth, yuvFormat, yuvRange, createAlpha, filePath.str().c_str()));
+
+    testutil::avifImagePtr image =
+        testutil::createImage(width, height, bitDepth, yuvFormat, createAlpha ? AVIF_PLANES_ALL : AVIF_PLANES_YUV, yuvRange);
+    ASSERT_NE(image, nullptr);
+    const uint32_t yuva[] = { (yuvRange == AVIF_RANGE_LIMITED) ? (235u << (bitDepth - 8)) : ((1u << bitDepth) - 1),
+                              (yuvRange == AVIF_RANGE_LIMITED) ? (240u << (bitDepth - 8)) : ((1u << bitDepth) - 1),
+                              (yuvRange == AVIF_RANGE_LIMITED) ? (240u << (bitDepth - 8)) : ((1u << bitDepth) - 1),
+                              (1u << bitDepth) - 1 };
+    testutil::fillImagePlain(image.get(), yuva);
+    ASSERT_TRUE(y4mWrite(filePath.str().c_str(), image.get()));
+
+    testutil::avifImagePtr decoded(avifImageCreateEmpty(), avifImageDestroy);
+    ASSERT_NE(decoded, nullptr);
+    ASSERT_TRUE(y4mRead(filePath.str().c_str(), decoded.get(), /*sourceTiming=*/nullptr, /*iter=*/nullptr));
+
+    EXPECT_TRUE(testutil::areImagesEqual(*image, *decoded));
 }
 
 INSTANTIATE_TEST_SUITE_P(OpaqueCombinations,
@@ -205,3 +73,4 @@ INSTANTIATE_TEST_SUITE_P(AlphaCombinations,
                                  /*createAlpha=*/Values(true)));
 
 } // namespace
+} // namespace libavif
