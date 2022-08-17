2f3b30e355d6eaebbb717f30244cb7d259d001a4
Wan-Teh Chang <wtc@google.com>
2022-08-16 17:03:26 -0700

    Add the avifDecoder option 'imageDimensionLimit'
    Add the avifDecoder option 'imageDimensionLimit', default to 32768. This
    allows the decoder to reject extra large image widths or heights by
    default. Note that our image size and dimension limits are essentially
    the same as Chrome media's limits:
    https://source.chromium.org/chromium/chromium/src/+/main:media/base/limits.h
    
      // Maximum possible dimension (width or height) for any video.
      constexpr int kMaxDimension = (1 << 15) - 1;  // 32767
    
      // Maximum possible canvas size (width multiplied by height) for any video.
      constexpr int kMaxCanvas = (1 << (14 * 2));  // 16384 x 16384
    
    Add the --dimension-limit option to avifdec.
    
    Add the internal utility function avifDimensionsTooLarge(), declared in
    include/avif/internal.h and defined in src/avif.c.
    
    Bug: b/242362974

--- include/avif/avif.h
+++ include/avif/avif.h
@@ -68,9 +68,12 @@ typedef int avifBool;
 
 #define AVIF_DIAGNOSTICS_ERROR_BUFFER_SIZE 256
 
-// A reasonable default for maximum image size to avoid out-of-memory errors or integer overflow in
-// (32-bit) int or unsigned int arithmetic operations.
+// A reasonable default for maximum image size (in pixel count) to avoid out-of-memory errors or
+// integer overflow in (32-bit) int or unsigned int arithmetic operations.
 #define AVIF_DEFAULT_IMAGE_SIZE_LIMIT (16384 * 16384)
+
+// A reasonable default for maximum image dimension (width or height).
+#define AVIF_DEFAULT_IMAGE_DIMENSION_LIMIT 32768
 
 // a 12 hour AVIF image sequence, running at 60 fps (a basic sanity check as this is quite ridiculous)
 #define AVIF_DEFAULT_IMAGE_COUNT_LIMIT (12 * 3600 * 60)
@@ -877,11 +880,16 @@ typedef struct avifDecoder
     avifBool ignoreExif;
     avifBool ignoreXMP;
 
-    // This represents the maximum size of a image (in pixel count) that libavif and the underlying
-    // AV1 decoder should attempt to decode. It defaults to AVIF_DEFAULT_IMAGE_SIZE_LIMIT, and can be
-    // set to a smaller value. The value 0 is reserved.
+    // This represents the maximum size of an image (in pixel count) that libavif and the underlying
+    // AV1 decoder should attempt to decode. It defaults to AVIF_DEFAULT_IMAGE_SIZE_LIMIT, and can
+    // be set to a smaller value. The value 0 is reserved.
     // Note: Only some underlying AV1 codecs support a configurable size limit (such as dav1d).
     uint32_t imageSizeLimit;
+
+    // This represents the maximum dimension of an image (width or height) that libavif should
+    // attempt to decode. It defaults to AVIF_DEFAULT_IMAGE_DIMENSION_LIMIT. Set it to 0 to ignore
+    // the limit.
+    uint32_t imageDimensionLimit;
 
     // This provides an upper bound on how many images the decoder is willing to attempt to decode,
     // to provide a bit of protection from malicious or malformed AVIFs citing millions upon
