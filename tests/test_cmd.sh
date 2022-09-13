82e6c72ad25370b6cf5b8fdff39f732ef7f33d33
Yannis Guyon <yguyon@google.com>
2022-09-13 11:48:01 +0200

    Use trap cleanup EXIT in test_cmd.sh

--- tests/test_cmd.sh
+++ tests/test_cmd.sh
@@ -37,8 +37,6 @@ else
   TMP_DIR=/tmp
 fi
 
-STATUS=0
-
 AVIFENC="${BINARY_DIR}/avifenc"
 AVIFDEC="${BINARY_DIR}/avifdec"
 ARE_IMAGES_EQUAL="${BINARY_DIR}/tests/are_images_equal"
@@ -47,25 +45,34 @@ ARE_IMAGES_EQUAL="${BINARY_DIR}/tests/are_images_equal"
 "${AVIFENC}" --version
 "${AVIFDEC}" --version
 
-pushd ${TMP_DIR}
-  # Input/output file paths.
-  INPUT_Y4M="${TESTDATA_DIR}/kodim03_yuv420_8bpc.y4m"
-  INPUT_PNG="${TESTDATA_DIR}/paris_icc_exif_xmp.png"
-  INPUT_JPG="${TESTDATA_DIR}/paris_exif_xmp_icc.jpg"
-  ENCODED_FILE="avif_test_cmd_encoded.avif"
-  ENCODED_FILE_NO_METADATA="avif_test_cmd_encoded_no_metadata.avif"
-  ENCODED_FILE_WITH_DASH="-avif_test_cmd_encoded.avif"
-  DECODED_FILE="avif_test_cmd_decoded.png"
-  DECODED_FILE_LOSSLESS="avif_test_cmd_decoded_lossless.png"
+# Input file paths.
+INPUT_Y4M="${TESTDATA_DIR}/kodim03_yuv420_8bpc.y4m"
+INPUT_PNG="${TESTDATA_DIR}/paris_icc_exif_xmp.png"
+INPUT_JPG="${TESTDATA_DIR}/paris_exif_xmp_icc.jpg"
+# Output file names.
+ENCODED_FILE="avif_test_cmd_encoded.avif"
+ENCODED_FILE_NO_METADATA="avif_test_cmd_encoded_no_metadata.avif"
+ENCODED_FILE_WITH_DASH="-avif_test_cmd_encoded.avif"
+DECODED_FILE="avif_test_cmd_decoded.png"
+DECODED_FILE_LOSSLESS="avif_test_cmd_decoded_lossless.png"
 
-  # Lossy test.
+# Cleanup
+cleanup() {
+  pushd ${TMP_DIR}
+    rm -- "${ENCODED_FILE}" "${ENCODED_FILE_NO_METADATA}" \
+          "${ENCODED_FILE_WITH_DASH}" "${DECODED_FILE}" "${DECODED_FILE_LOSSLESS}"
+  popd
+}
+trap cleanup EXIT
+
+pushd ${TMP_DIR}
+  # Lossy test. The decoded pixels should be different from the original image.
   echo "Testing basic lossy"
   "${AVIFENC}" -s 8 "${INPUT_Y4M}" -o "${ENCODED_FILE}"
   "${AVIFDEC}" "${ENCODED_FILE}" "${DECODED_FILE}"
-  "${ARE_IMAGES_EQUAL}" "${INPUT_Y4M}" "${DECODED_FILE}" 0 && \
-    echo "Error: Image should be different" && STATUS=1
+  "${ARE_IMAGES_EQUAL}" "${INPUT_Y4M}" "${DECODED_FILE}" 0 && exit 1
 
-  # Lossless test.
+  # Lossless test. The decoded pixels should be the same as the original image.
   echo "Testing basic lossless"
   # TODO(yguyon): Make this test pass with INPUT_PNG instead of DECODED_FILE.
   "${AVIFENC}" -s 10 -l "${DECODED_FILE}" -o "${ENCODED_FILE}"
@@ -73,32 +80,25 @@ pushd ${TMP_DIR}
   "${ARE_IMAGES_EQUAL}" "${DECODED_FILE}" "${DECODED_FILE_LOSSLESS}" 0
 
   # Argument parsing test with filenames starting with a dash.
+  echo "Testing arguments"
   "${AVIFENC}" -s 10 "${INPUT_PNG}" -- "${ENCODED_FILE_WITH_DASH}"
   "${AVIFDEC}" --info  -- "${ENCODED_FILE_WITH_DASH}"
   # Passing a filename starting with a dash without using -- should fail.
-  "${AVIFENC}" -s 10 "${INPUT_PNG}" "${ENCODED_FILE_WITH_DASH}" && \
-    echo "Error: Argument parsing should fail for avifenc" && STATUS=1
-  "${AVIFDEC}" --info "${ENCODED_FILE_WITH_DASH}" && \
-    echo "Error: Argument parsing should fail for avifdec" && STATUS=1
+  "${AVIFENC}" -s 10 "${INPUT_PNG}" "${ENCODED_FILE_WITH_DASH}" && exit 1
+  "${AVIFDEC}" --info "${ENCODED_FILE_WITH_DASH}" && exit 1
 
   # Metadata test.
   echo "Testing metadata enc/dec"
   for INPUT in "${INPUT_PNG}" "${INPUT_JPG}"; do
     "${AVIFENC}" "${INPUT}" -o "${ENCODED_FILE}"
+    # Ignoring a metadata chunk should produce a different output file.
     "${AVIFENC}" "${INPUT}" -o "${ENCODED_FILE_NO_METADATA}" --ignore-icc
-    cmp "${ENCODED_FILE}" "${ENCODED_FILE_NO_METADATA}" && \
-      echo "Error: --ignore-icc had no effect but should have had" && STATUS=1
+    cmp "${ENCODED_FILE}" "${ENCODED_FILE_NO_METADATA}" && exit 1
     "${AVIFENC}" "${INPUT}" -o "${ENCODED_FILE_NO_METADATA}" --ignore-exif
-    cmp "${ENCODED_FILE}" "${ENCODED_FILE_NO_METADATA}" && \
-      echo "Error: --ignore-exif had no effect but should have had" && STATUS=1
+    cmp "${ENCODED_FILE}" "${ENCODED_FILE_NO_METADATA}" && exit 1
     "${AVIFENC}" "${INPUT}" -o "${ENCODED_FILE_NO_METADATA}" --ignore-xmp
-    cmp "${ENCODED_FILE}" "${ENCODED_FILE_NO_METADATA}" && \
-      echo "Error: --ignore-xmp had no effect but should have had" && STATUS=1
+    cmp "${ENCODED_FILE}" "${ENCODED_FILE_NO_METADATA}" && exit 1
   done
-
-  # Cleanup
-  rm -- "${ENCODED_FILE}" "${ENCODED_FILE_NO_METADATA}" \
-        "${ENCODED_FILE_WITH_DASH}" "${DECODED_FILE}" "${DECODED_FILE_LOSSLESS}"
 popd
 
-exit "${STATUS}"
+exit 0
