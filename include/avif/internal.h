b96e5293d510a6795655ab46d1827a2718dabf23
Yannis Guyon <yguyon@google.com>
2022-10-27 13:04:48 +0200

    Simplify avifSetExifOrientation() comment

--- include/avif/internal.h
+++ include/avif/internal.h
@@ -215,7 +215,6 @@ avifResult avifImageExtractExifOrientationToIrotImir(avifImage * image);
 // Converts image->transformFlags, image->irot and image->imir to the equivalent Exif orientation value in [1:8].
 uint8_t avifImageGetExifOrientationFromIrotImir(const avifImage * image);
 // Attempts to parse the image->exif payload until the Exif orientation is found, then sets it to the given value.
-// Returns AVIF_RESULT_OK or an error.
 avifResult avifSetExifOrientation(avifRWData * exif, uint8_t orientation);
 
 // ---------------------------------------------------------------------------
