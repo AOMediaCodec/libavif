d7897e85daa675922494f50c0fce44c2dee68360
Yannis Guyon <yguyon@google.com>
2022-12-08 16:48:54 +0100

    Remove redundant test in avifopaquetest

--- tests/gtest/avifopaquetest.cc
+++ tests/gtest/avifopaquetest.cc
@@ -28,28 +28,5 @@ TEST(OpaqueTest, AlphaAndNoAlpha) {
   }
 }
 
-TEST(OpaqueTest, Gradient) {
-  for (int depth : {8, 10, 12}) {
-    // YUVA.
-    testutil::AvifImagePtr opaque_alpha = testutil::CreateImage(
-        1024, 1024, depth, AVIF_PIXEL_FORMAT_YUV444, AVIF_PLANES_ALL);
-    const uint32_t max_value = (1u << depth) - 1;
-    const uint32_t yuva[] = {max_value, max_value, max_value, max_value};
-    testutil::FillImagePlain(opaque_alpha.get(), yuva);
-
-    // View on YUV, to make sure the color values are identical.
-    testutil::AvifImagePtr no_alpha(avifImageCreateEmpty(), avifImageDestroy);
-    const avifCropRect rect = {0, 0, opaque_alpha->width, opaque_alpha->height};
-    ASSERT_EQ(avifImageSetViewRect(no_alpha.get(), opaque_alpha.get(), &rect),
-              AVIF_RESULT_OK);
-    avifImageFreePlanes(no_alpha.get(), AVIF_PLANES_A);
-
-    // Decorrelate YUV and A.
-    testutil::FillImageGradient(no_alpha.get());
-
-    EXPECT_TRUE(testutil::AreImagesEqual(*opaque_alpha, *no_alpha));
-  }
-}
-
 }  // namespace
 }  // namespace libavif
