d959c408b903f971d64b16902fb740bc58f4c7d2
Yannis Guyon <yguyon@google.com>
2023-03-31 14:03:21 +0200

    Use RGBImageFreePixels in RGBImageAllocatePixels

--- src/avif.c
+++ src/avif.c
@@ -581,13 +581,11 @@ void avifRGBImageSetDefaults(avifRGBImage * rgb, const avifImage * image)
 
 avifResult avifRGBImageAllocatePixels(avifRGBImage * rgb)
 {
-    if (rgb->pixels) {
-        avifFree(rgb->pixels);
-    }
-
-    rgb->rowBytes = rgb->width * avifRGBImagePixelSize(rgb);
-    rgb->pixels = avifAlloc((size_t)rgb->rowBytes * rgb->height);
+    avifRGBImageFreePixels(rgb);
+    const uint32_t rowBytes = rgb->width * avifRGBImagePixelSize(rgb);
+    rgb->pixels = avifAlloc((size_t)rowBytes * rgb->height);
     AVIF_CHECKERR(rgb->pixels, AVIF_RESULT_OUT_OF_MEMORY);
+    rgb->rowBytes = rowBytes;
     return AVIF_RESULT_OK;
 }
 
