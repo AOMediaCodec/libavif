d9b8042fa7a7b05acdd6864678304167ac46e2d6
Yannis Guyon <yguyon@google.com>
2023-05-09 17:24:05 +0200

    Add test_cmd_avm_lossless.sh

--- tests/test_cmd_avm_lossless.sh
+++ tests/test_cmd_avm_lossless.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2022 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -58,22 +58,22 @@ trap cleanup EXIT
 
 pushd ${TMP_DIR}
   # Generate test data.
-  "${AVIFENC}" -s 8 "${INPUT_PNG}" -o "${ENCODED_FILE}"
+  "${AVIFENC}" -c avm -s 8 "${INPUT_PNG}" -o "${ENCODED_FILE}"
   "${AVIFDEC}" "${ENCODED_FILE}" "${DECODED_FILE}"
 
   # Combining some arguments with lossless should fail.
   for option in "-y 400" "--min 0 --max 1" "-r limited" "--cicp 2/2/8"; do
-    "${AVIFENC}" $option -s 10 -l "${DECODED_FILE}" -o "${ENCODED_FILE}" && exit 1
+    "${AVIFENC}" -c avm $option -s 10 -l "${DECODED_FILE}" -o "${ENCODED_FILE}" && exit 1
   done
 
   # Combining some arguments with lossless should work.
   for option in "-y 444" "--min 0 --max 0" "-r full"; do
-    "${AVIFENC}" $option -s 10 -l "${DECODED_FILE}" -o "${ENCODED_FILE}"
+    "${AVIFENC}" -c avm $option -s 10 -l "${DECODED_FILE}" -o "${ENCODED_FILE}"
   done
 
   # Lossless test. The decoded pixels should be the same as the original image.
   echo "Testing basic lossless"
-  "${AVIFENC}" -s 10 -l "${INPUT_PNG}" -o "${ENCODED_FILE}"
+  "${AVIFENC}" -c avm -s 10 -l "${INPUT_PNG}" -o "${ENCODED_FILE}"
   "${AVIFDEC}" "${ENCODED_FILE}" "${DECODED_FILE_LOSSLESS}"
   "${ARE_IMAGES_EQUAL}" "${INPUT_PNG}" "${DECODED_FILE_LOSSLESS}" 0
 popd
