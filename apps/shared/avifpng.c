d93be466667762c851f02b5803d4f12cf2ee57d5
Wan-Teh Chang <wtc@google.com>
2023-06-08 20:27:07 -0700

    avifpng.c: Validate RGB row bytes in avifPNGRead()
    If libavif and libpng disagree on the value of RGB row bytes, this is a
    bug and we should not proceed.

--- apps/shared/avifpng.c
+++ apps/shared/avifpng.c
@@ -372,6 +372,11 @@ avifBool avifPNGRead(const char * inputFilename,
         fprintf(stderr, "Conversion to YUV failed: %s (out of memory)\n", inputFilename);
         goto cleanup;
     }
+    size_t rowBytes = png_get_rowbytes(png, info);
+    if (rgb.rowBytes != rowBytes) {
+        fprintf(stderr, "avifPNGRead internal error: rowBytes mismatch libavif %u vs libpng %" AVIF_FMT_ZU "\n", rgb.rowBytes, rowBytes);
+        goto cleanup;
+    }
     rowPointers = (png_bytep *)malloc(sizeof(png_bytep) * rgb.height);
     for (uint32_t y = 0; y < rgb.height; ++y) {
         rowPointers[y] = &rgb.pixels[y * rgb.rowBytes];
