590c36e00cf9c869a075aa78fb899f1c264c0259
Wan-Teh Chang <wtc@google.com>
2023-08-23 16:12:24 -0700

    Add "Version 1.0.0 ends here" comments
    Add "Version 1.0.0 ends here" comments to the end of the avifImage,
    avifDecoder, and avifEncoder structs. New members in future libavif
    1.x.y releases should be added to these structs after the "Version 1.0.0
    ends here" comments, followed by "Version 1.x.y ends here" comments.

--- include/avif/avif.h
+++ include/avif/avif.h
@@ -555,6 +555,8 @@ typedef struct avifImage
     // Metadata - set with avifImageSetMetadata*() before write, check .size>0 for existence after read
     avifRWData exif;
     avifRWData xmp;
+
+    // Version 1.0.0 ends here. Add any new members after this line.
 } avifImage;
 
 // avifImageCreate() and avifImageCreateEmpty() return NULL if arguments are invalid or if a memory allocation failed.
@@ -1006,6 +1008,8 @@ typedef struct avifDecoder
 
     // Internals used by the decoder
     struct avifDecoderData * data;
+
+    // Version 1.0.0 ends here. Add any new members after this line.
 } avifDecoder;
 
 AVIF_API avifDecoder * avifDecoderCreate(void);
@@ -1172,6 +1176,8 @@ typedef struct avifEncoder
     // Internals used by the encoder
     struct avifEncoderData * data;
     struct avifCodecSpecificOptions * csOptions;
+
+    // Version 1.0.0 ends here. Add any new members after this line.
 } avifEncoder;
 
 // avifEncoderCreate() returns NULL if a memory allocation failed.
