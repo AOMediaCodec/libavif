d225f28f86d68bd4ce6fc54fc1e4b0da3f3979bb
Yannis Guyon <yguyon@google.com>
2023-08-30 13:18:54 +0200

    Call WriteHDRProperties() in WriteExtendedMeta()

--- src/write.c
+++ src/write.c
@@ -1842,6 +1842,7 @@ static avifResult avifImageWriteExtendedMeta(const avifImage * imageMetadata, av
         AVIF_CHECKRES(avifRWStreamWriteBox(stream, "ipco", AVIF_BOX_SIZE_TBD, &ipco));
         {
             // No need for dedup because there is only one property of each type and for a single item.
+            AVIF_CHECKRES(avifEncoderWriteHDRProperties(stream, stream, imageMetadata, /*ipma=*/NULL, /*dedup=*/NULL));
             AVIF_CHECKRES(avifEncoderWriteExtendedColorProperties(stream, stream, imageMetadata, /*ipma=*/NULL, /*dedup=*/NULL));
         }
         avifRWStreamFinishBox(stream, ipco);
