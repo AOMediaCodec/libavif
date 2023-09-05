3347ba40624474cd4d1b01e8cab33a4080319554
Cacodemon345 <wahil1976@outlook.com>
2023-09-05 17:28:56 +0600

    Strip away and clean up imported libyuv files

--- third_party/libyuv/include/libyuv.h
+++ third_party/libyuv/include/libyuv.h
@@ -12,22 +12,10 @@
 #define INCLUDE_LIBYUV_H_
 
 #include "libyuv/basic_types.h"
-#include "libyuv/compare.h"
-#include "libyuv/convert.h"
-#include "libyuv/convert_argb.h"
-#include "libyuv/convert_from.h"
-#include "libyuv/convert_from_argb.h"
-#include "libyuv/cpu_id.h"
-#include "libyuv/mjpeg_decoder.h"
 #include "libyuv/planar_functions.h"
-#include "libyuv/rotate.h"
-#include "libyuv/rotate_argb.h"
 #include "libyuv/row.h"
 #include "libyuv/scale.h"
-#include "libyuv/scale_argb.h"
 #include "libyuv/scale_row.h"
-#include "libyuv/scale_uv.h"
 #include "libyuv/version.h"
-#include "libyuv/video_common.h"
 
 #endif  // INCLUDE_LIBYUV_H_
