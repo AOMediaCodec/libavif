a7a404cccc50dc3e25d684dfb1251bac0c1cf0aa
Frankie Dintino <fdintino@gmail.com>
2023-10-13 12:51:02 -0400

    fix(cmake): Track dependencies for merged static archive

--- cmake/Modules/merge_static_libs.cmake
+++ cmake/Modules/merge_static_libs.cmake
@@ -1,65 +1,83 @@
-function(merge_static_libs target)
-  set(args ${ARGN})
+function(merge_static_libs target unmerged_libs)
+    set(args ${ARGN})
 
-  foreach(lib ${args})
-    if("${lib}" MATCHES "(\\${CMAKE_STATIC_LIBRARY_SUFFIX}|dav1d\.a)$")
-      list(APPEND libs ${lib})
-    endif()
-  endforeach()
+    set(dependencies)
 
-  if(APPLE)
-    add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMENT "Merge static libraries with libtool"
-      COMMAND ${CMAKE_COMMAND} -E rename $<TARGET_FILE:${target}> $<TARGET_FILE:${target}>.tmp
-      COMMAND xcrun libtool -static -o $<TARGET_FILE:${target}> $<TARGET_FILE:${target}>.tmp ${libs}
-      COMMAND ${CMAKE_COMMAND} -E remove $<TARGET_FILE:${target}>.tmp
-    )
-  elseif(CMAKE_C_COMPILER_ID MATCHES "^(Clang|GNU|Intel|IntelLLVM)$")
-    add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMENT "Merge static libraries with ar"
-      COMMAND ${CMAKE_COMMAND} -E rename $<TARGET_FILE:${target}> $<TARGET_FILE:${target}>.tmp
-      COMMAND ${CMAKE_COMMAND} -E echo CREATE $<TARGET_FILE:${target}> >script.ar
-      COMMAND ${CMAKE_COMMAND} -E echo ADDLIB $<TARGET_FILE:${target}>.tmp >>script.ar
-    )
+    foreach(lib ${args})
+        if(TARGET "${lib}")
+            get_target_property(target_type ${lib} TYPE)
 
-    foreach(lib ${libs})
-      add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E echo ADDLIB ${lib} >>script.ar
-      )
+            if(${target_type} STREQUAL "STATIC_LIBRARY")
+                list(APPEND libs $<TARGET_FILE:${lib}>)
+                list(APPEND dependencies "${lib}")
+            else()
+                list(APPEND unmerged_libs "${lib}")
+            endif()
+        elseif("${lib}" MATCHES "(\\${CMAKE_STATIC_LIBRARY_SUFFIX}|dav1d\.a)$")
+            list(APPEND libs "${lib}")
+
+            if(EXISTS "${lib}")
+                list(APPEND dependencies "${lib}")
+            endif()
+        else()
+            list(APPEND unmerged_libs "${lib}")
+        endif()
     endforeach()
 
+    set(source_file ${CMAKE_CURRENT_BINARY_DIR}/${target}_depends.c)
+    add_library(${target} STATIC ${source_file})
+
     add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E echo SAVE >>script.ar
-      COMMAND ${CMAKE_COMMAND} -E echo END >>script.ar
-      COMMAND ${CMAKE_AR} -M <script.ar
-      COMMAND ${CMAKE_COMMAND} -E remove $<TARGET_FILE:${target}>.tmp script.ar
+        OUTPUT ${source_file} COMMAND ${CMAKE_COMMAND} -E echo "const int dummy = 0;" > ${source_file} DEPENDS ${dependencies}
     )
-  elseif(MSVC)
-    if(CMAKE_LIBTOOL)
-      set(BUNDLE_TOOL ${CMAKE_LIBTOOL})
+
+    add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E remove $<TARGET_FILE:${target}>)
+
+    if(APPLE)
+        add_custom_command(
+            TARGET ${target}
+            POST_BUILD
+            COMMENT "Merge static libraries with libtool"
+            COMMAND xcrun libtool -static -o $<TARGET_FILE:${target}> ${libs}
+        )
+    elseif(CMAKE_C_COMPILER_ID MATCHES "^(Clang|GNU|Intel|IntelLLVM)$")
+        add_custom_command(
+            TARGET ${target}
+            POST_BUILD
+            COMMENT "Merge static libraries with ar"
+            COMMAND ${CMAKE_COMMAND} -E echo CREATE $<TARGET_FILE:${target}> >script.ar
+        )
+
+        foreach(lib ${libs})
+            add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E echo ADDLIB ${lib} >>script.ar)
+        endforeach()
+
+        add_custom_command(
+            TARGET ${target}
+            POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E echo SAVE >>script.ar
+            COMMAND ${CMAKE_COMMAND} -E echo END >>script.ar
+            COMMAND ${CMAKE_AR} -M <script.ar
+            COMMAND ${CMAKE_COMMAND} -E remove script.ar
+        )
+    elseif(MSVC)
+        if(CMAKE_LIBTOOL)
+            set(BUNDLE_TOOL ${CMAKE_LIBTOOL})
+        else()
+            find_program(BUNDLE_TOOL lib HINTS "${CMAKE_C_COMPILER}/..")
+
+            if(NOT BUNDLE_TOOL)
+                message(FATAL_ERROR "Cannot locate lib.exe to bundle libraries")
+            endif()
+        endif()
+
+        add_custom_command(
+            TARGET ${target}
+            POST_BUILD
+            COMMENT "Merge static libraries with lib.exe"
+            COMMAND ${BUNDLE_TOOL} /NOLOGO /OUT:$<TARGET_FILE:${target}> ${libs}
+        )
     else()
-      find_program(BUNDLE_TOOL lib HINTS "${CMAKE_C_COMPILER}/..")
-
-      if(NOT BUNDLE_TOOL)
-        message(FATAL_ERROR "Cannot locate lib.exe to bundle libraries")
-      endif()
+        message(FATAL_ERROR "Unsupported platform for static link merging")
     endif()
-
-    add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMENT "Merge static libraries with lib.exe"
-      COMMAND ${CMAKE_COMMAND} -E rename $<TARGET_FILE:${target}> $<TARGET_FILE:${target}>.tmp
-      COMMAND ${BUNDLE_TOOL} /NOLOGO /OUT:$<TARGET_FILE:${target}> $<TARGET_FILE:${target}>.tmp ${libs}
-      COMMAND ${CMAKE_COMMAND} -E remove $<TARGET_FILE:${target}>.tmp
-    )
-  else()
-    message(FATAL_ERROR "Unsupported platform for static link merging")
-  endif()
 endfunction()
