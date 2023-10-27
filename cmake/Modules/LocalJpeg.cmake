93b53bcb1461e888a5c5568aa7c011389c7e89fb
Frankie Dintino <fdintino@gmail.com>
2023-10-27 18:11:48 -0400

    feat(cmake): Use FetchContent and ExternalProject for local deps

--- cmake/Modules/LocalJpeg.cmake
+++ cmake/Modules/LocalJpeg.cmake
@@ -1,16 +1,53 @@
+set(AVIF_LOCAL_JPEG_TAG "3.0.2")
+
+add_library(JPEG::JPEG STATIC IMPORTED GLOBAL)
+
 set(LIB_DIR "${AVIF_SOURCE_DIR}/ext/libjpeg-turbo/build.libavif")
 if(MSVC)
     set(LIB_FILENAME "${LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}jpeg-static${CMAKE_STATIC_LIBRARY_SUFFIX}")
 else()
     set(LIB_FILENAME "${LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}jpeg${CMAKE_STATIC_LIBRARY_SUFFIX}")
 endif()
-if(NOT EXISTS "${LIB_FILENAME}")
-    message(FATAL_ERROR "libavif: ${LIB_FILENAME} is missing, bailing out")
+if(EXISTS "${LIB_FILENAME}")
+    message(STATUS "libavif(AVIF_LOCAL_JPEG): ${LIB_FILENAME} found, using for local JPEG")
+    set(JPEG_INCLUDE_DIR "${AVIF_SOURCE_DIR}/ext/libjpeg-turbo")
+else()
+    message(STATUS "libavif(AVIF_LOCAL_JPEG): ${LIB_FILENAME} not found, fetching")
+    set(LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/libjpeg/src/libjpeg-build")
+    if(MSVC)
+        set(LIB_FILENAME "${LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}jpeg-static${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    else()
+        set(LIB_FILENAME "${LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}jpeg${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    endif()
+
+    set(JPEG_INSTALL_DIR "${prefix}/libjpeg-install")
+
+    ExternalProject_Add(
+        libjpeg
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/libjpeg
+        GIT_REPOSITORY "https://github.com/libjpeg-turbo/libjpeg-turbo.git"
+        GIT_TAG "${AVIF_LOCAL_JPEG_TAG}"
+        LIST_SEPARATOR |
+        BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config $<CONFIG> --target jpeg-static
+        CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+                   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                   -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+                   -DCMAKE_C_FLAGS_DEBUG=${CMAKE_C_FLAGS_DEBUG}
+                   -DCMAKE_C_FLAGS_RELEASE=${CMAKE_C_FLAGS_RELEASE}
+                   -DCMAKE_C_FLAGS_MINSIZEREL=${CMAKE_C_FLAGS_MINSIZEREL}
+                   -DCMAKE_C_FLAGS_RELWITHDEBINFO=${CMAKE_C_FLAGS_RELWITHDEBINFO}
+                   -DENABLE_SHARED=OFF
+                   -DENABLE_STATIC=ON
+                   -DCMAKE_BUILD_TYPE=Release
+                   -DWITH_TURBOJPEG=OFF
+        BUILD_BYPRODUCTS "${LIB_FILENAME}"
+        INSTALL_COMMAND ""
+    )
+    add_dependencies(JPEG::JPEG libjpeg)
+    set(JPEG_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/libjpeg/src/libjpeg)
 endif()
 
-add_library(JPEG::JPEG STATIC IMPORTED GLOBAL)
 set_target_properties(JPEG::JPEG PROPERTIES IMPORTED_LOCATION "${LIB_FILENAME}" AVIF_LOCAL ON)
-set(JPEG_INCLUDE_DIR "${AVIF_SOURCE_DIR}/ext/libjpeg-turbo")
 target_include_directories(JPEG::JPEG INTERFACE "${JPEG_INCLUDE_DIR}")
 
 # Also add the build directory path because it contains jconfig.h,
--- cmake/Modules/LocalLibargparse.cmake
+++ cmake/Modules/LocalLibargparse.cmake
@@ -1,10 +1,35 @@
+set(AVIF_LOCAL_LIBARGPARSE_GIT_TAG ee74d1b53bd680748af14e737378de57e2a0a954)
+
 set(LIBARGPARSE_FILENAME
     "${AVIF_SOURCE_DIR}/ext/libargparse/build/${CMAKE_STATIC_LIBRARY_PREFIX}argparse${CMAKE_STATIC_LIBRARY_SUFFIX}"
 )
+
 if(EXISTS "${LIBARGPARSE_FILENAME}")
+    message(STATUS "libavif(AVIF_LOCAL_LIBARGPARSE): compiled library found at ${LIBARGPARSE_FILENAME}")
     add_library(libargparse STATIC IMPORTED GLOBAL)
     set_target_properties(libargparse PROPERTIES IMPORTED_LOCATION "${LIBARGPARSE_FILENAME}" AVIF_LOCAL ON)
     target_include_directories(libargparse INTERFACE "${AVIF_SOURCE_DIR}/ext/libargparse/src")
 else()
-    message(WARNING "${LIBARGPARSE_FILENAME} is missing, not building avifgainmaputil, please run ext/libargparse.cmd")
+    message(STATUS "libavif(AVIF_LOCAL_LIBARGPARSE): compiled library not found at ${LIBARGPARSE_FILENAME}; using FetchContent")
+    if(EXISTS "${AVIF_SOURCE_DIR}/ext/libargparse")
+        message(STATUS "libavif(AVIF_LOCAL_LIBARGPARSE): ext/libargparse found; using as FetchContent SOURCE_DIR")
+        set(FETCHCONTENT_SOURCE_DIR_LIBARGPARSE "${AVIF_SOURCE_DIR}/ext/libargparse")
+        message(CHECK_START "libavif(AVIF_LOCAL_LIBARGPARSE): configuring libargparse")
+    else()
+        message(CHECK_START "libavif(AVIF_LOCAL_LIBARGPARSE): fetching and configuring libargparse")
+    endif()
+
+    FetchContent_Declare(
+        libargparse
+        GIT_REPOSITORY "https://github.com/kmurray/libargparse.git"
+        GIT_TAG ${AVIF_LOCAL_LIBARGPARSE_GIT_TAG}
+        UPDATE_COMMAND ""
+    )
+    avif_fetchcontent_populate_cmake(libargparse)
+
+    message(CHECK_PASS "complete")
+endif()
+
+if(EXISTS "${AVIF_SOURCE_DIR}/ext/libargparse")
+    set_target_properties(libargparse PROPERTIES FOLDER "ext/libargparse")
 endif()
