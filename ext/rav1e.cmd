c43e7f7070c96a8a8bbf3458929041e1a6b7f792
Frankie Dintino <fdintino@gmail.com>
2023-11-19 19:34:41 -0500

    fix: build cargo-c with --locked to work around rust-lang/cargo#13002

--- ext/rav1e.cmd
+++ ext/rav1e.cmd
@@ -13,7 +13,7 @@
 git clone -b v0.6.6 --depth 1 https://github.com/xiph/rav1e.git
 
 cd rav1e
-cargo install cargo-c
+cargo install cargo-c --locked
 
 mkdir build.libavif
 cargo cinstall --release --library-type=staticlib --prefix=/usr --destdir build.libavif
