f2dc0294e911a2880daea48894422d06be59b025
Wan-Teh Chang <wtc@google.com>
2023-12-03 17:45:51 -0800

    Declare prop var in avifPropertyArrayFind as const

--- src/read.c
+++ src/read.c
@@ -170,7 +170,7 @@ AVIF_ARRAY_DECLARE(avifPropertyArray, avifProperty, prop);
 static const avifProperty * avifPropertyArrayFind(const avifPropertyArray * properties, const char * type)
 {
     for (uint32_t propertyIndex = 0; propertyIndex < properties->count; ++propertyIndex) {
-        avifProperty * prop = &properties->prop[propertyIndex];
+        const avifProperty * prop = &properties->prop[propertyIndex];
         if (!memcmp(prop->type, type, 4)) {
             return prop;
         }
