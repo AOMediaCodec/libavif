9016cf9d1c9306c34bb397f3cefd2035d642cce1
Biswapriyo Nath <nathbappai@gmail.com>
2023-12-28 15:30:40 +0000

    pkgconf: Add yuv library in Libs.private for static linking
    libyuv was not added in Requires.private because it does not provide pkgconfig file officially

--- libavif.pc.cmake
+++ libavif.pc.cmake
@@ -7,6 +7,7 @@ Name: @PROJECT_NAME@
 Description: Library for encoding and decoding .avif files
 Version: @PROJECT_VERSION@
 Libs: -L${libdir} -lavif
+Libs.private:@AVIF_PKG_CONFIG_EXTRA_LIBS_PRIVATE@
 Cflags: -I${includedir}@AVIF_PKG_CONFIG_EXTRA_CFLAGS@
 Cflags.private: -UAVIF_DLL
 Requires.private:@AVIF_PKG_CONFIG_EXTRA_REQUIRES_PRIVATE@
