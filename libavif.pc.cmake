e9c99ed4fbad0328f50f4a31cea4c15207c6f245
Biswapriyo Nath <nathbappai@gmail.com>
2023-12-28 13:14:55 +0000

    pkgconf: add UAVIF_DLL for static linking with pkgconf
    allows for the usage of `pkgconf --static --cflags libavif' to produce
    the proper CFLAGS for static linking. Relies on pkgconf rather than pkg-config.

--- libavif.pc.cmake
+++ libavif.pc.cmake
@@ -8,3 +8,4 @@ Description: Library for encoding and decoding .avif files
 Version: @PROJECT_VERSION@
 Libs: -L${libdir} -lavif
 Cflags: -I${includedir}@AVIF_PKG_CONFIG_EXTRA_CFLAGS@
+Cflags.private: -UAVIF_DLL
