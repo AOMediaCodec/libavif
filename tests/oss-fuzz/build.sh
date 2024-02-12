2610dc4f12b17aff5d1b2bc3401f1b7c20f0be6f
Vincent Rabaud <vrabaud@google.com>
2024-02-12 17:01:53 +0100

    Merge avif_decode_fuzzer into avif_fuzztest_dec.cc
    This is the last non-fuzztest test to be migrated.

--- tests/oss-fuzz/build.sh
+++ tests/oss-fuzz/build.sh
@@ -71,12 +71,6 @@ cmake .. -G Ninja -DBUILD_SHARED_LIBS=OFF -DAVIF_CODEC_AOM=LOCAL -DAVIF_CODEC_DA
 
 ninja
 
-# build decode fuzzer
-$CXX $CXXFLAGS -std=c++11 -I../include \
-    ../tests/oss-fuzz/avif_decode_fuzzer.cc -o $OUT/avif_decode_fuzzer \
-    $LIB_FUZZING_ENGINE libavif.a ../ext/dav1d/build/src/libdav1d.a \
-    ../ext/libyuv/build/libyuv.a ../ext/aom/build.libavif/libaom.a
-
 # Restrict fuzztest tests to the only compatible fuzz engine: libfuzzer.
 if [ "$FUZZING_ENGINE" == "libfuzzer" ]
 then
@@ -113,6 +107,3 @@ fi
 mkdir $OUT/corpus
 unzip $SRC/avif_decode_seed_corpus.zip -d $OUT/corpus
 cp $SRC/libavif/tests/data/* $OUT/corpus
-
-# create a bigger seed corpus for avif_decode_fuzzer
-zip -j $OUT/avif_decode_fuzzer_seed_corpus.zip $OUT/corpus/*
