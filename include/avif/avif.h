6dea0a2dfc51e88591857c725d80c0aa68c4519c
Yannis Guyon <yguyon@google.com>
2024-04-17 17:03:30 +0200

    Add avifRange ranges in avif.h
    Taken from libaom.
    
    [skip ci]

--- include/avif/avif.h
+++ include/avif/avif.h
@@ -305,8 +305,12 @@ typedef enum avifChromaSamplePosition
 
 typedef enum avifRange
 {
-    AVIF_RANGE_LIMITED = 0,
-    AVIF_RANGE_FULL = 1
+    AVIF_RANGE_LIMITED = 0, /**<- Y  [16..235],  UV  [16..240]  (bit depth 8) */
+                            /**<- Y  [64..940],  UV  [64..960]  (bit depth 10) */
+                            /**<- Y [256..3760], UV [256..3840] (bit depth 12) */
+    AVIF_RANGE_FULL = 1     /**<- YUV/RGB [0..255]  (bit depth 8) */
+                            /**<- YUV/RGB [0..1023] (bit depth 10) */
+                            /**<- YUV/RGB [0..4095] (bit depth 12) */
 } avifRange;
 
 // ---------------------------------------------------------------------------
