3f25280c2abf19b2975fb8056fbff0e6ccfad376
Vincent Rabaud <vrabaud@google.com>
2024-04-17 17:13:59 +0200

    Revert changing zlib and libpng.
    libpng has upgraded its CMake which requires the zlib fix:
    https://github.com/madler/zlib/commit/f1f503da85d52e56aae11557b4d79a42bcaa2b86

--- cmake/Modules/LocalZlibpng.cmake
+++ cmake/Modules/LocalZlibpng.cmake
@@ -1,5 +1,5 @@
-set(AVIF_LOCAL_ZLIB_GIT_TAG v1.3.1)
-set(AVIF_LOCAL_LIBPNG_GIT_TAG v1.6.43)
+set(AVIF_LOCAL_ZLIB_GIT_TAG v1.3)
+set(AVIF_LOCAL_LIBPNG_GIT_TAG v1.6.40)
 
 if(EXISTS "${AVIF_SOURCE_DIR}/ext/zlib")
     message(STATUS "libavif(AVIF_ZLIBPNG=LOCAL): ext/zlib found; using as FetchContent SOURCE_DIR")
@@ -48,11 +48,13 @@ endif()
 target_include_directories(zlibstatic INTERFACE $<BUILD_INTERFACE:${ZLIB_INCLUDE_DIR}>)
 
 # This include_directories() call and the previous include_directories() call provide the zlib
-# include directories for add_subdirectory(ext/libpng). Because we set ZLIB_ROOT,
+# include directories for add_subdirectory(ext/libpng). Because we set PNG_BUILD_ZLIB,
 # libpng/CMakeLists.txt won't call find_package(ZLIB REQUIRED) and will see an empty
 # ${ZLIB_INCLUDE_DIR}.
 include_directories("${zlib_BINARY_DIR}")
 set(CMAKE_DEBUG_POSTFIX "")
+
+add_library(ZLIB::ZLIB ALIAS zlibstatic)
 
 message(CHECK_PASS "complete")
 
@@ -67,10 +69,10 @@ endif()
 # This is the only way I could avoid libpng going crazy if it found awk.exe, seems benign otherwise
 set(PREV_ANDROID ${ANDROID})
 set(ANDROID TRUE)
-set(ZLIB_ROOT "${zlib_SOURCE_DIR}" CACHE STRING "" FORCE)
+set(PNG_BUILD_ZLIB "${zlib_SOURCE_DIR}" CACHE STRING "" FORCE)
 set(PNG_SHARED OFF CACHE BOOL "")
 set(PNG_TESTS OFF CACHE BOOL "")
-set(PNG_TOOLS OFF CACHE BOOL "")
+set(PNG_EXECUTABLES OFF CACHE BOOL "")
 
 set(LIBPNG_BINARY_DIR "${FETCHCONTENT_BASE_DIR}/libpng")
 if(ANDROID_ABI)
