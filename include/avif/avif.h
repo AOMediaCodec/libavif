950a73c0c2ed728d35b0774e44d69a0de778e035
Yannis Guyon <yguyon@google.com>
2024-05-13 10:52:16 +0200

    Clarify range for RGB and alpha

--- include/avif/avif.h
+++ include/avif/avif.h
@@ -305,12 +305,13 @@ typedef enum avifChromaSamplePosition
 
 typedef enum avifRange
 {
+    // AVIF_RANGE_LIMITED is only applicable to YUV planes. RGB and alpha planes are always full range.
     AVIF_RANGE_LIMITED = 0, /**<- Y  [16..235],  UV  [16..240]  (bit depth 8) */
                             /**<- Y  [64..940],  UV  [64..960]  (bit depth 10) */
                             /**<- Y [256..3760], UV [256..3840] (bit depth 12) */
-    AVIF_RANGE_FULL = 1     /**<- YUV/RGB [0..255]  (bit depth 8) */
-                            /**<- YUV/RGB [0..1023] (bit depth 10) */
-                            /**<- YUV/RGB [0..4095] (bit depth 12) */
+    AVIF_RANGE_FULL = 1     /**<- [0..255]  (bit depth 8) */
+                            /**<- [0..1023] (bit depth 10) */
+                            /**<- [0..4095] (bit depth 12) */
 } avifRange;
 
 // ---------------------------------------------------------------------------
