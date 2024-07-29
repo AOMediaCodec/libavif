AOMediaCodec/libavif

commit f086d9857f689144affb7625be8acb9788cfea65

fix: link against transitive libaom library dependencies
fixes #2274

cmake/Modules/LocalAom.cmake
@@ -23,6 +23,27 @@ if(EXISTS "${LIB_FILENAME}")
         # ext/avm/aom/aom_encoder.h includes config/aom_config.h which is generated by the local build of avm.
         target_include_directories(aom INTERFACE "${AOM_EXT_SOURCE_DIR}/build.libavif")
     endif()
+
+    # Add link dependency flags from the aom.pc file in ext/aom or ext/avm
+    # by prepending the build directory to PKG_CONFIG_PATH and then calling
+    # pkg_check_modules
+    if(WIN32)
+        set(ENV{PKG_CONFIG_PATH} "${AOM_EXT_SOURCE_DIR}/build.libavif;$ENV{PKG_CONFIG_PATH}")
+    else()
+        set(ENV{PKG_CONFIG_PATH} "${AOM_EXT_SOURCE_DIR}/build.libavif:$ENV{PKG_CONFIG_PATH}")
+    endif()
+
+    pkg_check_modules(_AOM QUIET aom)
+
+    set(_AOM_PC_LIBRARIES "${_AOM_STATIC_LIBRARIES}")
+    # remove "aom" so we only have library dependencies
+    list(REMOVE_ITEM _AOM_PC_LIBRARIES "aom")
+
+    # Add absolute paths to libraries
+    foreach(_lib ${_AOM_PC_LIBRARIES})
+        find_library(_aom_dep_lib_${_lib} ${_lib} HINTS ${_AOM_STATIC_LIBRARY_DIRS})
+        target_link_libraries(aom INTERFACE ${_aom_dep_lib_${_lib}})
+    endforeach()
 else()
     message(STATUS "${AOM_MESSAGE_PREFIX}: compiled library not found at ${LIB_FILENAME}, using FetchContent")
     if(EXISTS "${AOM_EXT_SOURCE_DIR}")

cmake/Modules/Findaom.cmake
@@ -38,6 +38,16 @@ mark_as_advanced(AOM_INCLUDE_DIR AOM_LIBRARY AOM_LIBRARIES)
 if(AOM_LIBRARY)
     if("${AOM_LIBRARY}" MATCHES "\\${CMAKE_STATIC_LIBRARY_SUFFIX}$")
         add_library(aom STATIC IMPORTED GLOBAL)
+
+        set(_AOM_PC_LIBRARIES "${_AOM_STATIC_LIBRARIES}")
+        # remove "aom" so we only have library dependencies
+        list(REMOVE_ITEM _AOM_PC_LIBRARIES "aom")
+
+        # Add absolute paths to libraries
+        foreach(_lib ${_AOM_PC_LIBRARIES})
+            find_library(_aom_dep_lib_${_lib} ${_lib} HINTS ${_AOM_STATIC_LIBRARY_DIRS})
+            target_link_libraries(aom INTERFACE ${_aom_dep_lib_${_lib}})
+        endforeach()
     else()
         add_library(aom SHARED IMPORTED GLOBAL)
     endif()

commit ce27bd5d363aedb8a82531db2aac4b4875a43c84

Work around the bug that aom.pc always has -lm
Fix an issue introduced in
https://github.com/AOMediaCodec/libavif/pull/2304 when libaom is built
locally in the ext/ directory. See
https://github.com/AOMediaCodec/libavif/pull/2304#pullrequestreview-2203255217
for more information.

Related to https://github.com/AOMediaCodec/libavif/issues/2274.

cmake/Modules/LocalAom.cmake
@@ -38,6 +38,11 @@ if(EXISTS "${LIB_FILENAME}")
     set(_AOM_PC_LIBRARIES "${_AOM_STATIC_LIBRARIES}")
     # remove "aom" so we only have library dependencies
     list(REMOVE_ITEM _AOM_PC_LIBRARIES "aom")
+    # Work around crbug.com/aomedia/356153293: aom.pc has -lm in the
+    # Libs.private field on all platforms, whether libm or m.lib exists or not.
+    if(WIN32 OR APPLE)
+        list(REMOVE_ITEM _AOM_PC_LIBRARIES "m")
+    endif()
 
     # Add absolute paths to libraries
     foreach(_lib ${_AOM_PC_LIBRARIES})