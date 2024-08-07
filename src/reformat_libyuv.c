708ae6f5aa49f40ca360cd97d5af6da1c22ba1f9
Wan-Teh Chang <wtc@google.com>
2024-08-06 18:52:09 -0700

    Add comment about stride params of HalfFloatPlane

--- src/reformat_libyuv.c
+++ src/reformat_libyuv.c
@@ -1131,6 +1131,7 @@ avifResult avifRGBImageUnpremultiplyAlphaLibYUV(avifRGBImage * rgb)
 avifResult avifRGBImageToF16LibYUV(avifRGBImage * rgb)
 {
     const float scale = 1.0f / ((1 << rgb->depth) - 1);
+    // Note: Unlike CopyPlane_16, HalfFloatPlane wants the stride in bytes.
     const int result = HalfFloatPlane((const uint16_t *)rgb->pixels,
                                       rgb->rowBytes,
                                       (uint16_t *)rgb->pixels,
