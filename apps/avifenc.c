73a91d8c913fa3f2cdf4861b8113cd22221fcc34
Yannis Guyon <yguyon@google.com>
2024-11-22 16:46:33 +0100

    Move oddness check to CropRectRequiresUpsampling()
    Remove yuvFormat arg from avifCropRectFromCleanApertureBox().
    Make avifCropRectConvertCleanApertureBox() behave exactly as before.
    Fix comments.
    Update avifclaptest.
    Print warnings in avifdec.

--- apps/avifenc.c
+++ apps/avifenc.c
@@ -2290,10 +2290,9 @@ int main(int argc, char * argv[])
 
         // Validate clap
         avifCropRect cropRect;
-        avifBool upsampleBeforeCropping;
         avifDiagnostics diag;
         avifDiagnosticsClearError(&diag);
-        if (!avifCropRectFromCleanApertureBox(&cropRect, &upsampleBeforeCropping, &image->clap, image->width, image->height, image->yuvFormat, &diag)) {
+        if (!avifCropRectFromCleanApertureBox(&cropRect, &image->clap, image->width, image->height, &diag)) {
             fprintf(stderr,
                     "ERROR: Invalid clap: width:[%d / %d], height:[%d / %d], horizOff:[%d / %d], vertOff:[%d / %d] - %s\n",
                     (int32_t)image->clap.widthN,
@@ -2307,6 +2306,7 @@ int main(int argc, char * argv[])
                     diag.error);
             goto cleanup;
         }
+        // avifCropRectRequiresUpsampling() does not need to be called for clap validation.
     }
     if (irotAngle != 0xff) {
         image->transformFlags |= AVIF_TRANSFORM_IROT;
