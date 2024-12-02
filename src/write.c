5fd0303b477a76b5f90713ab8d44b4f068e57396
Wan-Teh Chang <wtc@google.com>
2024-12-02 15:56:23 -0800

    Run clang-format

--- src/write.c
+++ src/write.c
@@ -3232,25 +3232,25 @@ avifResult avifEncoderFinish(avifEncoder * encoder, avifRWData * output)
 
     avifBoxMarker ftyp;
     AVIF_CHECKRES(avifRWStreamWriteBox(&s, "ftyp", AVIF_BOX_SIZE_TBD, &ftyp));
-    AVIF_CHECKRES(avifRWStreamWriteChars(&s, majorBrand, 4));              // unsigned int(32) major_brand;
-    AVIF_CHECKRES(avifRWStreamWriteU32(&s, minorVersion));                 // unsigned int(32) minor_version;
-    AVIF_CHECKRES(avifRWStreamWriteChars(&s, "avif", 4));                  // unsigned int(32) compatible_brands[];
-    if (useAvioBrand) {                                                    //
-        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "avio", 4));              // ... compatible_brands[]
-    }                                                                      //
-    if (isSequence) {                                                      //
-        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "avis", 4));              // ... compatible_brands[]
-        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "msf1", 4));              // ... compatible_brands[]
-        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "iso8", 4));              // ... compatible_brands[]
-    }                                                                      //
-    AVIF_CHECKRES(avifRWStreamWriteChars(&s, "mif1", 4));                  // ... compatible_brands[]
-    AVIF_CHECKRES(avifRWStreamWriteChars(&s, "miaf", 4));                  // ... compatible_brands[]
-    if (isMA1B) {                                                          //
-        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "MA1B", 4));              // ... compatible_brands[]
-    }                                                                      //
-    if ((imageMetadata->depth == 8) || (imageMetadata->depth == 10)) {     //
-        if (imageMetadata->yuvFormat == AVIF_PIXEL_FORMAT_YUV444) {        //
-            AVIF_CHECKRES(avifRWStreamWriteChars(&s, "MA1A", 4));          // ... compatible_brands[]
+    AVIF_CHECKRES(avifRWStreamWriteChars(&s, majorBrand, 4));          // unsigned int(32) major_brand;
+    AVIF_CHECKRES(avifRWStreamWriteU32(&s, minorVersion));             // unsigned int(32) minor_version;
+    AVIF_CHECKRES(avifRWStreamWriteChars(&s, "avif", 4));              // unsigned int(32) compatible_brands[];
+    if (useAvioBrand) {                                                //
+        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "avio", 4));          // ... compatible_brands[]
+    }                                                                  //
+    if (isSequence) {                                                  //
+        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "avis", 4));          // ... compatible_brands[]
+        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "msf1", 4));          // ... compatible_brands[]
+        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "iso8", 4));          // ... compatible_brands[]
+    }                                                                  //
+    AVIF_CHECKRES(avifRWStreamWriteChars(&s, "mif1", 4));              // ... compatible_brands[]
+    AVIF_CHECKRES(avifRWStreamWriteChars(&s, "miaf", 4));              // ... compatible_brands[]
+    if (isMA1B) {                                                      //
+        AVIF_CHECKRES(avifRWStreamWriteChars(&s, "MA1B", 4));          // ... compatible_brands[]
+    }                                                                  //
+    if ((imageMetadata->depth == 8) || (imageMetadata->depth == 10)) { //
+        if (imageMetadata->yuvFormat == AVIF_PIXEL_FORMAT_YUV444) {    //
+            AVIF_CHECKRES(avifRWStreamWriteChars(&s, "MA1A", 4));      // ... compatible_brands[]
         }
     }
 #if defined(AVIF_ENABLE_EXPERIMENTAL_GAIN_MAP)
