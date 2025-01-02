e8370394840602e29c30bbeca9ed2fcbc80bf00d
Vincent Rabaud <vrabaud@google.com>
2025-01-02 12:55:21 +0100

    Now use AVIF_FUZZTEST with OFF/LOCAL possibilities

--- cmake/Modules/LocalFuzztest.cmake
+++ cmake/Modules/LocalFuzztest.cmake
@@ -1,14 +1,14 @@
-set(AVIF_LOCAL_FUZZTEST_TAG "078ea0871cc96d3a69bad406577f176a4fa14ae9")
+set(AVIF_FUZZTEST_TAG "078ea0871cc96d3a69bad406577f176a4fa14ae9")
 
 set(FUZZTEST_SOURCE_DIR "${AVIF_SOURCE_DIR}/ext/fuzztest")
 
 if(EXISTS "${FUZZTEST_SOURCE_DIR}")
-    message(STATUS "libavif(AVIF_LOCAL_FUZZTEST=ON): folder found at ${FUZZTEST_SOURCE_DIR}")
+    message(STATUS "libavif(AVIF_FUZZTEST=LOCAL): folder found at ${FUZZTEST_SOURCE_DIR}")
     set(FUZZTEST_BINARY_DIR "${FUZZTEST_SOURCE_DIR}/build.libavif")
 else()
-    message(STATUS "libavif(AVIF_LIBYUV=LOCAL): compiled library not found at ${LIB_FILENAME}; using FetchContent")
+    message(STATUS "libavif(AVIF_FUZZTEST=LOCAL): compiled library not found at ${LIB_FILENAME}; using FetchContent")
 
-    message(CHECK_START "libavif(AVIF_LOCAL_FUZZTEST=ON): configuring fuzztest")
+    message(CHECK_START "libavif(AVIF_FUZZTEST=LOCAL): configuring fuzztest")
 
     set(FUZZTEST_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/fuzztest-src")
     set(FUZZTEST_BINARY_DIR "${FETCHCONTENT_BASE_DIR}/fuzztest-build")
@@ -16,7 +16,7 @@ else()
         fuzztest
         GIT_REPOSITORY "https://github.com/google/fuzztest.git"
         BINARY_DIR "${FUZZTEST_BINARY_DIR}"
-        GIT_TAG "${AVIF_LOCAL_FUZZTEST_TAG}"
+        GIT_TAG "${AVIF_FUZZTEST_TAG}"
         # Fixes for https://github.com/google/fuzztest/issues/1124
         PATCH_COMMAND
             sed -i.bak -e "s/-fsanitize=address//g" cmake/FuzzTestFlagSetup.cmake && sed -i.bak -e "s/-DADDRESS_SANITIZER//g"
--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -1,4 +1,4 @@
 set(CTEST_CUSTOM_TESTS_IGNORE
-    # Ignore failing tests brought by `add_subdirectory(${AVIF_SOURCE_DIR}/ext/fuzztest)` when AVIF_LOCAL_FUZZTEST is ON
+    # Ignore failing tests brought by `add_subdirectory(${AVIF_SOURCE_DIR}/ext/fuzztest)` when AVIF_FUZZTEST is not OFF
     antlr4_tests_NOT_BUILT
 )
