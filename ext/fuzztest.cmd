01a1d0a7a83454bf23fac81054641c73be7445cc
Vincent Rabaud <vrabaud@google.com>
2025-01-06 13:09:58 +0100

    Bump fuzztest

--- ext/fuzztest.cmd
+++ ext/fuzztest.cmd
@@ -9,8 +9,8 @@
 
 git clone https://github.com/google/fuzztest.git
 cd fuzztest
-: # There is no tagged release as of 2024/06/21. Pick the latest commit that works.
-git checkout 078ea0871cc96d3a69bad406577f176a4fa14ae9
+: # There is no tagged release as of 2024/01/06. Pick the latest commit that works.
+git checkout 3b4a590f7fc75a77823580c4c4e19d1c7bd6da52
 : # Fixes for https://github.com/google/fuzztest/issues/1124
 sed -i 's/-fsanitize=address//g' ./cmake/FuzzTestFlagSetup.cmake
 sed -i 's/-DADDRESS_SANITIZER//g' ./cmake/FuzzTestFlagSetup.cmake
