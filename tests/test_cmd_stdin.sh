f15cbd4041823fbaf33ae9ded15ff2393a5497a7
James Zern <jzern@google.com>
2025-03-11 19:29:46 -0700

    dump aliases

--- tests/test_cmd_stdin.sh
+++ tests/test_cmd_stdin.sh
@@ -37,8 +37,9 @@ strip_header_if() {
   if ${STRIP_HEADER}; then
     export
     grep --version
+    alias
+    export LC_ALL=C
     grep -b -m 1 -o --text mdat "${FILE}" || true
-    grep -h -b -m 1 -o --text mdat "${FILE}" || true
     od -tx1 -a "${FILE}"
     MDAT_OFFSET=$(grep -b -m 1 -o --text mdat "${FILE}" | cut -d: -f 1)
     dd if="${FILE}" of="${FILE}.strip" bs=1 skip="${MDAT_OFFSET}"
