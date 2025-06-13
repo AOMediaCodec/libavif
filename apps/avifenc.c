57b08fed9417e05429dd5c52082d4bcdc8a0f5a4
Wan-Teh Chang <wtc@google.com>
2025-06-13 12:07:18 -0700

    avifenc.c: Call quantizerToQuality() where we can
    I somehow missed this in
    https://github.com/AOMediaCodec/libavif/pull/2823.
    
    Also add a comment to quantizerToQuality() about the bias toward better
    quality in the calculation of the average quantizer.

--- apps/avifenc.c
+++ apps/avifenc.c
@@ -1430,6 +1430,8 @@ static void avifInputAdd(avifInput * input, const char * filePath, uint64_t dura
 
 static int quantizerToQuality(int minQuantizer, int maxQuantizer)
 {
+    // When calculating the average quantizer, discard the fractional part for a
+    // bias toward better quality.
     const int quantizer = (minQuantizer + maxQuantizer) / 2;
     const int quality = ((63 - quantizer) * 100 + 31) / 63;
     return quality;
@@ -2263,8 +2265,7 @@ int main(int argc, char * argv[])
                 if (fileSettings->minQuantizer.set) {
                     assert(fileSettings->maxQuantizer.set);
                     if (!fileSettings->quality.set) {
-                        const int quantizer = (fileSettings->minQuantizer.value + fileSettings->maxQuantizer.value) / 2;
-                        const int quality = ((63 - quantizer) * 100 + 31) / 63;
+                        const int quality = quantizerToQuality(fileSettings->minQuantizer.value, fileSettings->maxQuantizer.value);
                         fileSettings->quality = intSettingsEntryOf(quality);
                     }
                 } else {
@@ -2279,8 +2280,8 @@ int main(int argc, char * argv[])
                 if (fileSettings->minQuantizerAlpha.set) {
                     assert(fileSettings->maxQuantizerAlpha.set);
                     if (!fileSettings->qualityAlpha.set) {
-                        const int quantizerAlpha = (fileSettings->minQuantizerAlpha.value + fileSettings->maxQuantizerAlpha.value) / 2;
-                        const int qualityAlpha = ((63 - quantizerAlpha) * 100 + 31) / 63;
+                        const int qualityAlpha =
+                            quantizerToQuality(fileSettings->minQuantizerAlpha.value, fileSettings->maxQuantizerAlpha.value);
                         fileSettings->qualityAlpha = intSettingsEntryOf(qualityAlpha);
                     }
                 } else {
