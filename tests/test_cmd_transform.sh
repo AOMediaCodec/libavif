0b82b480e41349883dfe050250a27cd61c6ea52d
Wan-Teh Chang <wtc@google.com>
2026-03-21 10:02:17 -0700

    Revert "Skip ImageMagick test in MinGW"
    This reverts commit b3be0b951f11deed2603b339b799037a65bba818.
    
    Remove workaround for issue
    https://github.com/AOMediaCodec/libavif/issues/3111.
    
    The MinGW ImageMagick package bug has been fixed:
    https://github.com/msys2/MINGW-packages/issues/28550

--- tests/test_cmd_transform.sh
+++ tests/test_cmd_transform.sh
@@ -95,15 +95,6 @@ pushd ${TMP_DIR}
   fi
   "${IMAGEMAGICK}" --version
 
-  # Avoid ImageMagick 7.1.2-17 errors in MinGW. See
-  # https://github.com/AOMediaCodec/libavif/issues/3111.
-  if [[ -n "${MSYSTEM:-}" ]]
-  then
-      echo "Skipping ImageMagick test in MinGW"
-      popd
-      exit 0
-  fi
-
   JPEG_PSNR_THRESHOLD=48
 
   # Crop
