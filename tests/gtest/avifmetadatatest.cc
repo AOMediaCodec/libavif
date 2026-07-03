aa3150b394d07ce784d2281407b538b1affdbbc9
Wan-Teh Chang <wtc@google.com>
2026-07-03 12:32:30 -0700

    Add a test

--- tests/gtest/avifmetadatatest.cc
+++ tests/gtest/avifmetadatatest.cc
@@ -215,6 +215,14 @@ TEST(MetadataTest, DecoderParseICC) {
   EXPECT_EQ(decoder->image->icc.data[2], 2);
   EXPECT_EQ(decoder->image->icc.data[3], 84);
   avifDecoderDestroy(decoder);
+
+  decoder = avifDecoderCreate();
+  ASSERT_NE(decoder, nullptr);
+  decoder->ignoreColorProfile = true;
+  EXPECT_EQ(avifDecoderSetIOFile(decoder, file_path.c_str()), AVIF_RESULT_OK);
+  EXPECT_EQ(avifDecoderParse(decoder), AVIF_RESULT_OK);
+  ASSERT_EQ(decoder->image->icc.size, 0u);
+  avifDecoderDestroy(decoder);
 }
 
 //------------------------------------------------------------------------------
